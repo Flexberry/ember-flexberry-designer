80799512bd8b2d39e95604ad9e6e2717d9271599
i-Living <living.59@yandex.ru>
2017-09-21 18:06:18 +0500

    Update file names

--- addon/locales/en/components/fd-visual-control.js
+++ addon/locales/en/components/fd-visual-control.js
@@ -1,7 +1,7 @@
 export default {
+  prototypeBy: 'Prototype by',
   name: 'Name',
   type: 'Type',
   isNull: 'Null',
-  defaultValue: 'Default value',
-  readonly: 'Readonly',
+  defaultValue: 'Default value'
 };
