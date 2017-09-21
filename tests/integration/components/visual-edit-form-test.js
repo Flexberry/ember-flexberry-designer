428c81bf5773fcccec9552258c1093cc1d83dcf4
i-Living <living.59@yandex.ru>
2017-09-21 14:26:30 +0500

    Fix component tests

--- tests/integration/components/visual-edit-form-test.js
+++ tests/integration/components/visual-edit-form-test.js
@@ -1,8 +1,29 @@
+import Ember from 'ember';
+
+import I18nService from 'ember-i18n/services/i18n';
+import I18nRuLocale from 'ember-flexberry/locales/ru/translations';
+import I18nEnLocale from 'ember-flexberry/locales/en/translations';
+
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('visual-edit-form', 'Integration | Component | visual edit form', {
-  integration: true
+  integration: true,
+
+  beforeEach: function () {
+    this.register('locale:ru/translations', I18nRuLocale);
+    this.register('locale:en/translations', I18nEnLocale);
+    this.register('service:i18n', I18nService);
+
+    this.inject.service('i18n', { as: 'i18n' });
+    Ember.Component.reopen({
+      i18n: Ember.inject.service('i18n')
+    });
+
+    // Set 'ru' as initial locale.
+    this.set('i18n.locale', 'ru');
+  }
+});
 });
 
 test('it renders', function(assert) {
@@ -11,7 +32,7 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{visual-edit-form}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.notEqual(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
@@ -20,5 +41,5 @@ test('it renders', function(assert) {
     {{/visual-edit-form}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.notEqual(this.$().text().trim(), 'template block text');
 });
