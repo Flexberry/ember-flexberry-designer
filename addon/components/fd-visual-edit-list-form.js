20cb8a371c4d13fd574f56833880d3a2bdb3e082
Alexey Kostarev <kaf@ics.perm.ru>
2017-09-29 14:46:09 +0500

    Select form id form name

--- addon/components/fd-visual-edit-list-form.js
+++ addon/components/fd-visual-edit-list-form.js
@@ -4,8 +4,24 @@ import layout from '../templates/components/fd-visual-edit-list-form';
 export default Ember.Component.extend({
   layout,
 
+  store: Ember.inject.service('store'),
+
   init: function() {
     this._super();
-    let i=0;
+  },
+
+  actions: {
+    showForm: function() {
+      let store = this.get('store');
+      let select = Ember.$('#selectForm').get(0);
+      let option = select.options[select.selectedIndex];
+      let formId = option.value;
+      let formName = option.innerText;
+
+
+    }
+
   }
+
+
 });
