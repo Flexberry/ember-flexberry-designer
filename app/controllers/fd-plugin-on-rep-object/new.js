832664621c44f7d251b59a05593df89cb688e83c
Bratchikov Igor <bratchikov@ics.perm.ru>
2017-10-20 11:21:16 +0500

    Remove unnecessary new.js

--- app/controllers/fd-plugin-on-rep-object/new.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-flexberry-designer/controllers/fd-plugin-on-rep-object/new';
--- app/routes/s-t-o-r-m-c-a-s-e-s-t-o-r-m-n-e-t-repository-parameter-modifier/new.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-flexberry-designer/routes/s-t-o-r-m-c-a-s-e-s-t-o-r-m-n-e-t-repository-parameter-modifier/new';
