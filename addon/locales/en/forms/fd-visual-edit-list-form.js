1d6b42bd7887cf4f957062618f013536bee49b32
Eugene Kolegov <ekolegov@outlook.com>
2017-11-16 19:35:01 +0500

    Feature list-form-constructor small fixes for beauty

--- addon/locales/en/forms/fd-visual-edit-list-form.js
+++ addon/locales/en/forms/fd-visual-edit-list-form.js
@@ -1,11 +1,12 @@
 export default {
-  caption: 'Edit list form',
+  caption: 'The list form constructor',
   name: 'Name',
   description: 'Description',
 
   'edit-form-header': 'Edit form: ',
 
   'add-button-text': 'Add',
+  'preview-mobile-variant': 'Preview mobile variant',
 
   'listform-forward': 'Next',
   'listform-back': 'Back',
@@ -13,5 +14,4 @@ export default {
   'move-left-attr': 'Move left',
   'move-right-attr': 'Move right',
   'remove-attr': 'Remove attribute',
-
 };
--- addon/locales/ru/forms/fd-visual-edit-list-form.js
+++ addon/locales/ru/forms/fd-visual-edit-list-form.js
@@ -1,10 +1,12 @@
 export default {
-  caption: 'Редактирование списковой формы',
-  name: 'Наименование',
-  description: '',
+  caption: 'Конструктор списковой формы',
+  name: 'Название',
+  description: 'Описание',
+
   'edit-form-header': 'Форма для редактирования: ',
 
   'add-button-text': 'Добавить',
+  'preview-mobile-variant': 'Предпросмотр мобильного варианта',
 
   'listform-forward': 'Вперед',
   'listform-back': 'Назад',
@@ -12,5 +14,4 @@ export default {
   'move-left-attr': 'Переместить влево',
   'move-right-attr': 'Переместить вправо',
   'remove-attr': 'Удалить атрибут',
-
 };
