c33d9365a6bfb3f0cffb3b27dbe83b72cad5fbd4
Sokolov Andrey <s.andrey-2011@yandex.ru>
2017-11-16 18:21:52 +0500

    Update template stage

--- addon/locales/ru/forms/fd-stage-edit-form.js
+++ addon/locales/ru/forms/fd-stage-edit-form.js
@@ -5,5 +5,37 @@ export default {
   'company-caption': 'Company',
   'copyright-caption': 'Copyright',
   'product-caption': 'Product',
-  'configuration-caption': ''
+  'configuration-caption': '',
+  'ember': {
+    'caption': 'Flexberry Ember',
+    'frontend': {
+      'caption': 'Frontend',
+      'git-url': 'Git репозиторий URL',
+      'login': 'Логин',
+      'password': 'Пароль или токен',
+      'branch': 'Ветка',
+      'publish': 'Опубликовать в gh-pages'
+    },
+    'backend': {
+      'caption': 'Backend',
+      'git-url': 'Git репозиторий URL',
+      'login': 'Логин',
+      'password': 'Пароль или токен',
+      'branch': 'Ветка'
+    },
+    'mobile': {
+      'caption': 'Mobile',
+      'cordova': 'Генерировать приложение Apache Cordova'
+    }
+  },
+  'bpm': {
+    'caption': 'Flexberry BPM',
+    'process-methodology': 'Использовать процессную методику',
+    'console-address': 'Адрес консоли установленной процессной части приложения'
+  },
+  'orm': {
+    'caption': 'Flexberry ORM',
+    'default-storage': 'Хранилище по уполчанию',
+    'load': 'Загрузка'
+  }
 };
