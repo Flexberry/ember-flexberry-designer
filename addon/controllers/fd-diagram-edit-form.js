caf8057206429342dfb75ff7fba68fe8fb89b928
Alexey Kostarev <kaf@ics.perm.ru>
2017-12-01 18:20:35 +0500

    Parsing PrimitivesJsonString into primitivs into setupController

--- addon/controllers/fd-diagram-edit-form.js
+++ addon/controllers/fd-diagram-edit-form.js
@@ -2,4 +2,7 @@ import EditFormController from 'ember-flexberry/controllers/edit-form';
 
 export default EditFormController.extend({
   parentRoute: 'fd-diagram-list-form',
+  init: function() {
+    let i=0;
+  }
 });
--- addon/routes/fd-diagram-edit-form.js
+++ addon/routes/fd-diagram-edit-form.js
@@ -1,6 +1,17 @@
 import EditFormRoute from 'ember-flexberry/routes/edit-form';
 
+import { Query } from 'ember-flexberry-data';
+const { Builder, FilterOperator } = Query;
+
 export default EditFormRoute.extend({
-  modelProjection: 'EditFormView',
-  modelName: 'fd-dev-uml-cad'
+  modelProjection: 'FdUmlCad',
+  modelName: 'fd-dev-uml-cad',
+
+
+  setupController: function (controller, model) {
+  this._super(...arguments);
+    let primitivesJsonString = model.get('primitivesJsonString');
+    model.set('primitives', eval(primitivesJsonString));
+  }
+
 });
