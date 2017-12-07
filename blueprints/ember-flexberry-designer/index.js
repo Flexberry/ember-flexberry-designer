5af24d35c8a6fe8aae9595e670e841f49bd1227c
Evgeniy Khaberev <ehaberev@yandex.ru>
2017-12-07 15:59:00 +0500

    Add `browserify.js`

--- blueprints/ember-flexberry-designer/index.js
+++ blueprints/ember-flexberry-designer/index.js
@@ -4,7 +4,7 @@ module.exports = {
     var _this = this;
     return _this.addAddonsToProject({
       packages: [
-        { name: 'ember-flexberry', target: '0.9.2-beta.7' }
+        { name: 'ember-flexberry', target: '0.9.2-beta.9' }
       ]
     });
   },
