16b5687b9630357e5c3525f605cb3fa44b930e04
Eugene Kolegov <ekolegov@outlook.com>
2018-04-11 10:51:42 +0500

    Fix fd-appstruct-list-form end editing by enter

--- addon/controllers/fd-appstruct-list-form.js
+++ addon/controllers/fd-appstruct-list-form.js
@@ -364,16 +364,20 @@ export default Ember.Controller.extend(FlexberryTreenodeActionsHandlerMixin, {
       inputElement.style.display = 'inline-block';
       inputElement.style.position = 'absolute';
       inputElement.style.padding = '6px 0px 0px 2px';
-      inputElement.onblur = inputElement.onkeypress = function (event) {
-        if (!event.keyCode || event.keyCode === 13) {
-          let inputElement = event.target;
-          let caption = inputElement.value;
-          let textElement = document.createTextNode(caption);
-          inputElement.parentNode.replaceChild(textElement, inputElement);
-          let _this = inputElement._this;
-          let node = _this._findNodeByPath(_this, inputElement.lastClickedPath);
-          Ember.set(node, 'caption', caption);
+      inputElement.onkeypress = (event) => {
+        if (event.keyCode === 13) {
+          event.target.blur();
         }
+      };
+
+      inputElement.onblur = function (event) {
+        let inputElement = event.target;
+        let caption = inputElement.value;
+        let textElement = document.createTextNode(caption);
+        inputElement.parentNode.replaceChild(textElement, inputElement);
+        let _this = inputElement._this;
+        let node = _this._findNodeByPath(_this, inputElement.lastClickedPath);
+        Ember.set(node, 'caption', caption);
       };
 
       textElement.parentNode.replaceChild(inputElement, textElement);
