dd7a8a146d6e1514905375f9279642ef69d7f580
Evgeniy Khaberev <ehaberev@yandex.ru>
2018-04-12 11:28:41 +0500

    Uncomment temporarily changes

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -61,14 +61,11 @@ export default Ember.Controller.extend({
         caption: i18n.t('forms.application.sitemap.root.fd-generation-process-form.caption'),
         title: i18n.t('forms.application.sitemap.root.fd-generation-process-form.title'),
       });
-
-      //TODO: Commented temporarily. Uncomment when needed.
-      // sitemap.nodes.push({
-      //   link: 'fd-system-list-form',
-      //   caption: i18n.t('forms.application.sitemap.root.fd-system-list-form.caption'),
-      //   title: i18n.t('forms.application.sitemap.root.fd-system-list-form.title'),
-      // });
-
+      sitemap.nodes.push({
+        link: 'fd-system-list-form',
+        caption: i18n.t('forms.application.sitemap.root.fd-system-list-form.caption'),
+        title: i18n.t('forms.application.sitemap.root.fd-system-list-form.title'),
+      });
       sitemap.nodes.push({
         link: 'fd-diagram-list-form',
         caption: i18n.t('forms.application.sitemap.root.fd-diagram-list-form.caption'),
