1b5c39ef0f82e4cc3b258f081925631dbb2511b7
Bratchikov Igor <bratchikov@ics.perm.ru>
2018-04-13 19:06:25 +0500

    Single stage mode

--- app/services/fd-current-project-context.js
+++ app/services/fd-current-project-context.js
@@ -1,1 +1,10 @@
-export { default } from 'ember-flexberry-designer/services/fd-current-project-context';
+import Ember from 'ember';
+import FdCurrentProjectContext from 'ember-flexberry-designer/services/fd-current-project-context';
+import config from '../config/environment';
+
+let singleModeStageId = Ember.get(config, 'APP.fdCurrentProjectContext.singleModeStageId');
+FdCurrentProjectContext.reopen({
+  singleModeStageId: singleModeStageId
+});
+
+export default FdCurrentProjectContext;
