9da4ffd2815f6e7a6c5c23e2dc76ef0a2dd1042f
Bratchikov Igor <bratchikov@ics.perm.ru>
2018-04-14 15:43:10 +0500

    Update app struct form route and controller

--- addon/routes/fd-appstruct-form.js
+++ addon/routes/fd-appstruct-form.js
@@ -12,7 +12,7 @@ export default Ember.Route.extend({
     let modelHash = {
       implementations: undefined,
       forms: undefined,
-      application: undefined
+      applications: undefined
     };
 
     // TODO: сразу вычитывать нужные структуры без необходимости переделки их на клиенте. Пусть будет несколько параллельных запросов на сервер.
@@ -54,117 +54,12 @@ export default Ember.Route.extend({
       where(applicationPredicate);
 
       let promiseApplication = _this.store.query('fd-dev-class', builderApplication.build()).then((result) => {
-        modelHash.application = result;
+        modelHash.applications = result;
       });
 
-      Ember.RSVP.all([/* promiseImplementation, promiseForms, */ promiseApplication]).then(() => {
+      Ember.RSVP.all([ /* promiseImplementation, promiseForms,*/ promiseApplication]).then(() => {
         resolve(modelHash);
       });
     });
   },
-
-  /* 
-  setupController: function (controller, model) {
-    let startTime = performance.now();
-    let applicationRecordId = null;
-    let itemList = [];
-    let leftParents = [];
-    let leftLeaves = [];
-    let n = model.get('length');
-    for (let i = 0; i < n; i++) {
-      let record = model.nextObject(i);
-      switch (record.get('stereotype')) {
-        case null:
-          leftParents.push({ id: record.get('id'), name: record.get('name'), description: record.get('description') });
-          break;
-        case '«listform»':
-        case '«editform»':
-          let formView = record.get('formViews').nextObject(0);
-          if (formView) {
-            let parentId = formView.get('view.class.id');
-            leftLeaves.push({
-              id: record.get('id'),
-              stereotype: record.get('stereotype'),
-              parentId:parentId,
-              name: record.get('name'),
-              description: record.get('description')
-            });
-          }
-
-          break;
-        case '«application»':
-          let recordId = record.get('id');
-          applicationRecordId = recordId;
-          itemList = record.get('containersStr');
-          break;
-        default:
-          break;
-      }
-    }
-
-    let leftTreeNodes = this._getLeftTree(leftParents, leftLeaves);
-
-    while (itemList && itemList.length === 1 && itemList[0].caption === 'Рабочий стол') {
-      itemList = itemList[0].nodes;
-    }
-
-    let rightTreeNodes = [{
-      caption:'Рабочий стол',
-      nodes: itemList
-    }];
-    model = {
-      id: applicationRecordId,
-      jsonLeftTreeNodes: leftTreeNodes,
-      jsonRightTreeNodes: rightTreeNodes
-    };
-    controller.initLeftTree(leftTreeNodes);
-    controller.initRightTree(rightTreeNodes);
-
-    let endTime = performance.now();
-
-    console.log('setupController time: ' + (endTime - startTime));
-
-    return this._super(controller, model);
-  },
-
-  _getLeftTree: function(leftParents, leftLeaves) {
-    leftParents.sort(
-      function(a, b) {
-        let ret = (a.id > b.id) ? 1 : ((a.id < b.id) ? -1 : 0);
-        return ret;
-      }
-    );
-    leftLeaves.sort(
-      function(a, b) {
-        let ret = (a.parentId > b.parentId) ? 1 : ((a.parentId < b.parentId) ? -1 :
-        ((a.name > b.name) ? 1 : ((a.name < b.name) ? -1 : 0)));
-        return ret;
-      }
-    );
-    let leftNodes = [];
-    let leaveIndex = 0;
-    for (let i = 0; i < leftParents.length; i++) {
-      let leftParent = leftParents[i];
-      let leftNode = {
-        id: leftParent.id,
-        caption: leftParent.name,
-        description: leftParent.description,
-        nodes: []
-      };
-      for (; leaveIndex < leftLeaves.length && leftLeaves[leaveIndex].parentId === leftParent.id; leaveIndex++) {
-        let leafLeaf = leftLeaves[leaveIndex];
-        leftNode.nodes.push({
-          id: leafLeaf.id,
-          stereotype: leafLeaf.stereotype,
-          caption: leafLeaf.name,
-          description: leafLeaf.description
-        });
-      }
-
-      leftNodes.push(leftNode);
-    }
-
-    return leftNodes;
-  }
-*/
 });
