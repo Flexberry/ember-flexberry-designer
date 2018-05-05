c4676262d63f1c6cefa64d352c2ed50392d94181
Bratchikov Igor <bratchikov@ics.perm.ru>
2018-05-05 09:17:26 +0500

    Add fd-editform-constructor route

---
+++ addon/controllers/fd-editform-constructor/new.js
@@ -0,0 +1,4 @@
+import FdEditformConstructorController from '../fd-editform-constructor';
+
+var FdEditformConstructorNewController = FdEditformConstructorController;
+export default FdEditformConstructorNewController;
