c9541239c0fc56fcb3ec179b06a1f9e50b8abaaf
Eugene Kolegov <ekolegov@outlook.com>
2018-05-29 12:54:46 +0500

    Feature fd-editform-group object definition

---
+++ addon/objects/fd-editform-group.js
@@ -0,0 +1,37 @@
+/**
+  @module ember-flexberry-designer
+*/
+
+import Ember from 'ember';
+
+/**
+  Describes a group of controls on the edit form.
+
+  @class FdEditformGroup
+  @extends <a href="http://emberjs.com/api/classes/Ember.Object.html">Ember.Object</a>
+*/
+export default Ember.Object.extend({
+  /**
+    Caption of the group.
+
+    @property caption
+    @type String
+  */
+  caption: undefined,
+
+  /**
+    Specifies the width of the group, in percent or in pixels.
+
+    @property width
+    @type String
+  */
+  width: undefined,
+
+  /**
+    A collection of rows with controls on the tab.
+
+    @property rows
+    @type FdEditformRow[]
+  */
+  rows: undefined,
+});
