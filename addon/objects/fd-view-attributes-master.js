0488f94d4439937fc268a12e2711fa6ae28ec236
Sokolov Andrey <s.andrey-2011@yandex.ru>
2018-06-01 15:52:09 +0500

    Add object model and update transforms view

---
+++ addon/objects/fd-view-attributes-master.js
@@ -0,0 +1,38 @@
+/**
+  @module ember-flexberry-designer
+*/
+
+import FdViewAttributesProperty from './fd-view-attributes-property';
+
+/**
+  Describes properties on the master view.
+
+  @class FdViewAttributesMaster
+  @extends <a href="http://emberjs.com/api/classes/Ember.Object.html">Ember.Object</a>
+*/
+export default FdViewAttributesProperty.extend({
+
+  /**
+    Item property 'lookupType'.
+
+    @property lookupType
+    @type String
+  */
+  lookupType: undefined,
+
+  /**
+    Item property 'masterPropertyName'.
+
+    @property masterPropertyName
+    @type String
+  */
+  masterPropertyName: undefined,
+
+  /**
+    Item property 'masterCustomixationString'.
+
+    @property masterCustomixationString
+    @type String
+  */
+  masterCustomixationString: undefined,
+});
--- addon/transforms/fd-definition.js
+++ addon/transforms/fd-definition.js
@@ -1,8 +1,12 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+import FdViewAttributesProperty from '../objects/fd-view-attributes-property';
+import FdViewAttributesMaster from '../objects/fd-view-attributes-master';
+import FdViewAttributesDatail from '../objects/fd-view-attributes-datail';
 
 export default DS.Transform.extend({
   deserialize(definition) {
-    let ret = [];
+    let ret = Ember.A();
     let parser = new DOMParser();
     let xmlDoc = parser.parseFromString(definition, 'text/xml');
     if (xmlDoc) {
@@ -12,22 +16,55 @@ export default DS.Transform.extend({
         if (viewPropertiesList.length > 0) {
           let itemList = viewPropertiesList[0].getElementsByTagName('Item');
           for (let item of itemList) {
+            let itemObject;
             let propertyName = item.getAttribute('PropertyName');
             let caption = item.getAttribute('Caption');
-            let visible =  item.getAttribute('Visible');
-            let isMaster =  item.getAttribute('IsMaster');
-            let lookupType =  item.getAttribute('LookupType');
-            let masterPropertyName =  item.getAttribute('MasterPropertyName');
-            let masterCustomizationString =  item.getAttribute('MasterCustomizationString');
-            ret.push({
-              propertyName:propertyName,
-              caption:caption,
-              visible: visible,
-              isMaster: isMaster,
-              lookupType: lookupType,
-              masterPropertyName: masterPropertyName,
-              masterCustomizationString: masterCustomizationString
-            });
+            let path = item.getAttribute('Path');
+            let visible =  Boolean(item.getAttribute('Visible'));
+            if (Boolean(item.getAttribute('IsMaster'))) {
+              let lookupType =  item.getAttribute('LookupType');
+              let masterPropertyName =  item.getAttribute('MasterPropertyName');
+              let masterCustomizationString =  item.getAttribute('MasterCustomizationString');
+              itemObject = FdViewAttributesMaster.create({
+                name:propertyName,
+                caption:caption,
+                path:path,
+                visible: visible,
+                lookupType: lookupType,
+                masterPropertyName: masterPropertyName,
+                masterCustomizationString: masterCustomizationString
+              });
+            } else {
+              itemObject = FdViewAttributesProperty.create({
+                name:propertyName,
+                caption:caption,
+                path:path,
+                visible: visible,
+              });
+            }
+
+            ret.push(itemObject);
+          }
+        }
+
+        let viewDetailsList = view[0].getElementsByTagName('ViewDetailsList');
+        if (viewDetailsList.length > 0) {
+          let itemList = viewDetailsList[0].getElementsByTagName('Item');
+          for (let item of itemList) {
+            let detailName = item.getAttribute('DetailName');
+            let detailViewName = item.getAttribute('DetailViewName');
+            let loadOnLoadAgregator = Boolean(item.getAttribute('LoadOnLoadAgregator'));
+            let detailPath =  item.getAttribute('DetailPath');
+            let detailCaption =  item.getAttribute('DetailCaption');
+            let detailVisible =  Boolean(item.getAttribute('DetailVisible'));
+            ret.push(FdViewAttributesDatail.create({
+              name:detailName,
+              detailViewName:detailViewName,
+              loadOnLoadAgregator:loadOnLoadAgregator,
+              path: detailPath,
+              caption: detailCaption,
+              visible: detailVisible
+            }));
           }
         }
       }
@@ -42,18 +79,38 @@ export default DS.Transform.extend({
     }
 
     let viewPropertiesList = '';
+    let viewDetailsList = '';
     for (let i = 0; i < deserialized.length; i++) {
       let d = deserialized[i];
-      let propertyName = `PropertyName="${d.propertyName}"`;
-      let caption = `Caption="${d.caption}"`;
-      let visible = `Visible="${d.visible === 'True' ? 'True' : 'False'}"`;
-      let isMaster = `IsMaster="${d.isMaster === 'True' ? 'True' : 'False'}"`;
-      let lookupType = `LookupType="${d.lookupType}"`;
-      let masterPropertyName = `MasterPropertyName="${d.masterPropertyName}"`;
-      let masterCustomizationString = `MasterCustomizationString="${d.masterCustomizationString}"`;
-      viewPropertiesList += `<Item ${propertyName} ${caption} ${visible} ${isMaster} ${lookupType} ${masterPropertyName} ${masterCustomizationString} />`;
+      if (d instanceof FdViewAttributesDatail) {
+        let detailName = `DetailName="${d.name}"`;
+        let detailViewName = `DetailViewName="${d.detailViewName}"`;
+        let loadOnLoadAgregator = `LoadOnLoadAgregator="${d.loadOnLoadAgregator}"`;
+        let detailPath =  `DetailPath="${d.path}"`;
+        let detailCaption =  `DetailCaption="${d.caption}"`;
+        let detailVisible = `DetailVisible="${d.visible}"`;
+        viewDetailsList += `<Item ${detailName} ${detailViewName} ${loadOnLoadAgregator} ${detailPath} ${detailCaption} ${detailVisible} />`;
+      } else {
+        let isMaster = `IsMaster="False"`;
+        let lookupType = `LookupType=""`;
+        let masterPropertyName = `MasterPropertyName=""`;
+        let masterCustomizationString = `MasterCustomizationString=""`;
+        if (d instanceof FdViewAttributesMaster) {
+          isMaster = `IsMaster="True"`;
+          lookupType = `LookupType="${d.lookupType}"`;
+          masterPropertyName = `MasterPropertyName="${d.masterPropertyName}"`;
+          masterCustomizationString = `MasterCustomizationString="${d.masterCustomizationString}"`;
+        }
+
+        let propertyName = `PropertyName="${d.name}"`;
+        let caption = `Caption="${d.caption}"`;
+        let path = `Path="${d.path}"`;
+        let visible = `Visible="${d.visible === 'True' ? 'True' : 'False'}"`;
+        viewPropertiesList += `<Item ${propertyName} ${caption} ${path} ${visible}
+         ${isMaster} ${lookupType} ${masterPropertyName} ${masterCustomizationString} />`;
+      }
     }
 
-    return `<View><ViewPropertiesList>${viewPropertiesList}</ViewPropertiesList><ViewDetailsList /></View>`;
+    return `<View><ViewPropertiesList>${viewPropertiesList}</ViewPropertiesList><ViewDetailsList>${viewDetailsList}</ViewDetailsList></View>`;
   }
 });
