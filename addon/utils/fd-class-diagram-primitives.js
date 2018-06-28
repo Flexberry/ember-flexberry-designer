53dea3c48e29c98c77c45a014cc8c3522fc53778
kpepelyaeva <pepelyaevaks@gmail.com>
2018-06-28 12:35:23 +0500

    Add primitives in util

--- addon/utils/fd-class-diagram-primitives.js
+++ addon/utils/fd-class-diagram-primitives.js
@@ -10,16 +10,19 @@ joint.shapes.basic.Generic.define('flexberryUml.Class', {
 
     '.uml-class-name-text': {
       'ref': '.uml-class-name-rect',
-      'ref-y': .5,
-      'ref-x': .5,
+      'ref-y': 0.5,
+      'ref-x': 0.5,
       'text-anchor': 'middle',
       'y-alignment': 'middle',
       'font-weight': 'bold',
       'fill': 'black',
       'font-size': 12,
-      'font-family': 'Arial'
+      'font-family': 'Arial',
     },
+
+    '.uml-class-name-text tspan[x]': { 'font-weight': 'normal' },
     '.uml-class-attrs-text': {
+
       'ref': '.uml-class-attrs-rect', 'ref-y': 5, 'ref-x': 5,
       'fill': 'black', 'font-size': 12, 'font-family': 'Arial'
     },
@@ -30,6 +33,7 @@ joint.shapes.basic.Generic.define('flexberryUml.Class', {
   },
 
   name: [],
+  stereotype:[],
   attributes: [],
   methods: []
 }, {
@@ -55,7 +59,8 @@ joint.shapes.basic.Generic.define('flexberryUml.Class', {
   },
 
   getClassName: function() {
-    return this.get('name');
+    let stereotype = this.get('stereotype').length > 0 ? '<<' + this.get('stereotype') + '>>' : '';
+    return [this.get('name'), stereotype];
   },
 
   updateRectangles: function() {
@@ -82,4 +87,152 @@ joint.shapes.basic.Generic.define('flexberryUml.Class', {
       offsetY += rectHeight;
     });
   }
+});
+
+joint.shapes.flexberryUml.Class.define('flexberryUml.Object', {
+  attrs: {
+    '.uml-class-name-text': {
+      'text-decoration': 'underline'
+    },
+  },
+}, {
+  markup: [
+    '<g class="rotatable">',
+    '<g class="scalable">',
+    '<rect class="uml-class-name-rect"/><rect class="uml-class-attrs-rect"/>',
+    '</g>',
+    '<text class="uml-class-name-text"/><text class="uml-class-attrs-text"/>',
+    '</g>'
+  ].join(''),
+
+  getClassName: function() {
+    return [this.get('name')];
+  },
+});
+
+joint.shapes.basic.Rhombus.define('flexberryUml.NAryAssociation', {
+  attrs: {
+    text: {
+      'text-decoration': 'underline',
+      'font-size':'12'
+    }
+  }
+});
+
+joint.shapes.basic.Rect.define('flexberryUml.Instance', {
+  attrs: {
+    text: {
+      'text-decoration': 'underline',
+      'font-size':'12'
+    }
+  }
+});
+
+joint.shapes.basic.Rect.define('flexberryUml.MultiObject', {
+  attrs: {
+    text: {
+      'text-decoration': 'underline',
+      'font-size':'12'
+    }
+  }
+}, {
+  markup: [
+    '<g class="rotatable">',
+    '<g class="scalable">',
+    '<rect transform="translate(3, 10)"/><rect/>',
+    '</g>',
+    '<text/>',
+    '</g>'
+  ].join('')
+});
+
+joint.shapes.flexberryUml.Instance.define('flexberryUml.ActiveObject', {
+  attrs: {
+    text: {
+      'font-weight':'bold'
+    }
+  }
+});
+
+joint.dia.Link.define('flexberryUml.Aggregation', {
+  attrs: { '.marker-target': { d: 'M 26 10 L 13 17 L 0 10 L 13 3 z', fill: 'white' } },
+  labels: [{
+    position: { distance: 10, offset: -15 }, attrs: { text: { text:  '*' } } }, {
+    position: { distance: -40, offset: -15 }, attrs: { text: { text:  '1' } }
+  }]
+});
+
+joint.dia.Link.define('flexberryUml.Qualified', {
+  attrs: { '.marker-target': { d: 'M 26 10 L 26 3 L 0 3 L 0 17 L 26 17 z', fill: 'white' } },
+});
+
+joint.dia.Link.define('flexberryUml.QualifiedAggregation', {
+  attrs: {
+    '.marker-target': { d: 'M 26 10 L 26 3 L 0 3 L 0 17 L 26 17 L 26 10 M 52 10 L 39 17 L 26 10 L 39 3 z', fill: 'white' }
+  },
+});
+
+joint.dia.Link.define('flexberryUml.QualifiedComposition', {
+  attrs: {
+    '.marker-target': { d: 'M 26 10 L 26 3 L 0 3 L 0 17 L 26 17 L 26 10 M 52 10 L 39 17 L 26 10 L 39 3 z', fill: 'url(#solids)' }
+  },
+}, {
+
+  initialize: function() {
+    // called from Backbone constructor
+    // call base initialize()
+    joint.dia.Link.prototype.initialize.apply(this, arguments);
+
+    // link markup is so complex that we need to fetch its definition
+    var markup = (this.markup || this.get('markup'));
+
+    // append <linearGradient> to markup, so that it covers whole path
+    markup += '<linearGradient id="solids" x1="0%" y1="0%" x2="100%" y2="0%">' +
+      '<stop offset="0%" style="stop-color:rgb(255,255,255);stop-opacity:1" />' +
+      '<stop offset="50%" style="stop-color:rgb(255,255,255);stop-opacity:1" />' +
+      '<stop offset="50%" style="stop-color:rgb(0,0,0);stop-opacity:1" />' +
+      '<stop offset="100%" style="stop-color:rgb(0,0,0);stop-opacity:1" />' +
+      '</linearGradient>';
+    this.set('markup', markup);
+  }
+});
+
+joint.dia.Link.define('flexberryUml.Composition', {
+  attrs: { '.marker-target': { d: 'M 26 10 L 13 17 L 0 10 L 13 3 z', fill: 'black' } },
+  labels: [{
+    position: { distance: 10, offset: -15 }, attrs: { text: { text:  '*' } } }, {
+    position: { distance: -40, offset: -15 }, attrs: { text: { text:  '1' } }
+  }]
+});
+
+joint.dia.Link.define('flexberryUml.Dependency', {
+  attrs: {
+    '.marker-target': { d: 'M 0 10 L 13 17 L 0 10 L 13 3 z', fill: 'black' },
+    '.connection': { stroke: 'black', 'stroke-width': 1, 'stroke-dasharray': '7 2' },
+  },
+});
+
+joint.dia.Link.define('flexberryUml.Realization', {
+  attrs: {
+    '.marker-target': { d: 'M 0 0 z' },
+    '.connection': { stroke: 'black', 'stroke-width': 1, 'stroke-dasharray': '7 2' },
+  },
+});
+
+joint.dia.Link.define('flexberryUml.Association', {
+  labels: [{
+    position: { distance: 10, offset: -15 }, attrs: { text: { text:  '*' } } }, {
+    position: { distance: -10, offset: -15 }, attrs: { text: { text:  '1' } }
+  }]
+});
+
+joint.dia.Link.define('flexberryUml.Generalization', {
+  attrs: { '.marker-target': { d: 'M 20 0 L 0 10 L 20 20 z', fill: 'white' } }
+});
+
+joint.dia.Link.define('flexberryUml.NestedClassAssociation', {
+  attrs: { '.marker-target': { d: 'M 10, 10 ' +
+                'm -8, 0 ' +
+                'a 8,8 0 1,0 16,0 ' +
+                'a 8,8 0 1,0 -16,0 M 10 2 L 10 2 L 10 18', fill: 'transparent' } }
 });
--- tests/dummy/app/controllers/class-diagram-primitives-demo.js
+++ tests/dummy/app/controllers/class-diagram-primitives-demo.js
@@ -30,27 +30,124 @@ export default Ember.Controller.extend({
         model: _this.graph,
       });
 
-      let attributes = ['attr1','attr2'];
-      let methods = ['method1'];
-      let jCadClass = new joint.shapes.flexberryUml.Class({
-        position: { x: 100, y: 100 },
-        size: { width: 100, height: 100 },
-        name: 'CaseName',
-        attributes: attributes,
-        methods: methods,
-        ports: {
-          groups: {
-            'in': {
-              position: 'absolute'
-            },
-            'out': {
-              position: 'absolute'
-            },
-          }
-      	},
+      var linkAggregation = new joint.shapes.flexberryUml.Aggregation({
+        source: { x:100, y:30 },
+        target: { x:300, y:30 },
       });
 
-      _this.graph.addCell(jCadClass);
+      var linkAssociation = new joint.shapes.flexberryUml.Association({
+        source: { x:100, y:80 },
+        target: { x:300, y:80 },
+        labels: [{
+          attrs: { text: { text:  '*' } } }, {
+          attrs: { text: { text:  '0..1' } }
+        }]
+      });
+
+      var linkComposition = new joint.shapes.flexberryUml.Composition({
+        source: { x:100, y:120 },
+        target: { x:300, y:120 },
+      });
+
+      var linkDependency = new joint.shapes.flexberryUml.Dependency({
+        source: { x:100, y:170 },
+        target: { x:300, y:170 },
+      });
+
+      var linkRealization = new joint.shapes.flexberryUml.Realization({
+        source: { x:100, y:220 },
+        target: { x:300, y:220 },
+      });
+
+      var linkGeneralization = new joint.shapes.flexberryUml.Generalization({
+        source: { x:100, y:270 },
+        target: { x:300, y:270 },
+      });
+
+      var linkQualified = new joint.shapes.flexberryUml.Qualified({
+        source: { x:100, y:320 },
+        target: { x:300, y:320 },
+      });
+
+      var linkQualifiedAggregation = new joint.shapes.flexberryUml.QualifiedAggregation({
+        source: { x:100, y:370 },
+        target: { x:300, y:370 },
+      });
+
+      var linkQualifiedComposition = new joint.shapes.flexberryUml.QualifiedComposition({
+        source: { x:100, y:420 },
+        target: { x:300, y:420 },
+      });
+      var linkNestedClassAssociation = new joint.shapes.flexberryUml.NestedClassAssociation({
+        source: { x:100, y:470 },
+        target: { x:300, y:470 },
+      });
+
+      _this.graph.addCell([linkAggregation, linkAssociation, linkComposition, linkDependency,
+        linkRealization, linkGeneralization, linkQualified, linkQualifiedAggregation,
+        linkQualifiedComposition, linkNestedClassAssociation]);
+
+      let attributes = ['attr1', 'attr2'];
+      let methods = ['method1'];
+
+      let classWithoutStp = new joint.shapes.flexberryUml.Class({
+        position: { x: 350, y: 30 },
+        size: { width: 100, height: 100 },
+        name: 'Class1',
+        attributes: attributes,
+        methods: methods,
+      });
+
+      let classWithStp = new joint.shapes.flexberryUml.Class({
+        position: { x: 350, y: 150 },
+        size: { width: 100, height: 100 },
+        name: 'Class2',
+        stereotype: 'enumeration',
+        attributes: attributes,
+        methods: methods,
+      });
+
+      let nAryAssociation = new joint.shapes.flexberryUml.NAryAssociation({
+        position: { x: 550, y: 30 },
+        size: { width: 150, height: 75 },
+        attrs: {
+          text: { text: 'n-ary Association' }
+        }
+      });
+
+      let obj = new joint.shapes.flexberryUml.Object({
+        position: { x: 350, y: 300 },
+        size: { width: 100, height: 70 },
+        name: 'Object',
+        attributes: attributes,
+        methods: methods,
+      });
+
+      var instance = new joint.shapes.flexberryUml.Instance({
+        position: { x: 350, y: 400 },
+        size: { width: 150, height: 40 },
+        attrs: {
+          text: { text: 'Instance' }
+        }
+      });
+
+      var multiObject = new joint.shapes.flexberryUml.MultiObject({
+        position: { x: 350, y: 500 },
+        size: { width: 150, height: 40 },
+        attrs: {
+          text: { text: 'multiObject' }
+        }
+      });
+
+      var activeObj = new joint.shapes.flexberryUml.ActiveObject({
+        position: { x: 350, y: 600 },
+        size: { width: 150, height: 40 },
+        attrs: {
+          text: { text: 'Active object' }
+        }
+      });
+
+      _this.graph.addCell([classWithoutStp, classWithStp, nAryAssociation, obj, instance, multiObject, activeObj]);
     });
   },
 });
