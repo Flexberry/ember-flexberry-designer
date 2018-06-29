8b94f88b11a0557169d44520ed5611af1baebaf5
Eugene Kolegov <ekolegov@outlook.com>
2018-06-29 13:58:42 +0500

    Feature fd-editform-constructor controller find container of any item

--- addon/controllers/fd-editform-constructor.js
+++ addon/controllers/fd-editform-constructor.js
@@ -54,10 +54,10 @@ export default Ember.Controller.extend({
     @private
     @property _selectedItemContainer
     @readOnly
-    @type Ember.NativeArray
+    @type Ember.NativeArray|FdEditformRow|FdEditformGroup|FdEditformTabgroup|FdEditformTab
   */
   _selectedItemContainer: Ember.computed('selectedItem', function() {
-    return this._findItemContainer(this.get('selectedItem'), this.get('model.controls'));
+    return this._findItemContainer(this.get('selectedItem'));
   }).readOnly(),
 
   /**
@@ -193,7 +193,7 @@ export default Ember.Controller.extend({
     if (selectedItem instanceof FdEditformRow) {
       target = selectedItem.get('controls');
     } else if (selectedItem instanceof FdEditformControl) {
-      target = this.get('_selectedItemContainer');
+      target = this.get('_selectedItemContainer.controls');
       index = target.indexOf(selectedItem) + 1;
     } else if (selectedItem instanceof FdEditformGroup || selectedItem instanceof FdEditformTab) {
       target = selectedItem.get('rows');
@@ -221,109 +221,34 @@ export default Ember.Controller.extend({
   /**
     Looks for a container that contains the item.
 
-    @param {FdEditformRow|FdEditformControl} item The sought item.
-    @param {Ember.Array} container The root container.
-    @return {Ember.Array} The container that was found or `null`.
+    @method _findItemContainer
+    @param {FdEditformRow|FdEditformControl|FdEditformGroup|FdEditformTabgroup|FdEditformTab} item
+      The sought item.
+    @param {Ember.NativeArray|FdEditformRow|FdEditformGroup|FdEditformTabgroup|FdEditformTab} [container]
+      The container from which to start the search, if not specified, uses `model.controls`.
+    @return {Ember.NativeArray|FdEditformRow|FdEditformGroup|FdEditformTabgroup|FdEditformTab}
+      The container that was found or `null`.
   */
-  _findItemContainer(item, container) {
-    if (item instanceof FdEditformRow) {
-      return this._findRowContainer(item, container);
-    } else {
-      return this._findControlContainer(item, container);
-    }
-  },
-
-  /**
-    Looks for a container that contains the row.
-
-    @method _findRowContainer
-    @param {FdEditformRow} row The sought row.
-    @param {Ember.Array} container The root container.
-    @return {Ember.Array} The container that was found or `null`.
-  */
-  _findRowContainer(row, container) {
-    let _container = null;
-    if (container.indexOf(row) === -1) {
+  _findItemContainer(item, container = this.get('model.controls')) {
+    let foundContainer;
+    if (container instanceof FdEditformControl) {
+      foundContainer = null;
+    } else if (container instanceof FdEditformRow && container.get('controls').indexOf(item) === -1) {
+      foundContainer = this._findItemContainer(item, container.get('controls'));
+    } else if (container instanceof FdEditformTabgroup && container.get('tabs').indexOf(item) === -1) {
+      foundContainer = this._findItemContainer(item, container.get('tabs'));
+    } else if (container instanceof FdEditformGroup || container instanceof FdEditformTab && container.get('rows').indexOf(item) === -1) {
+      foundContainer = this._findItemContainer(item, container.get('rows'));
+    } else if (Ember.isArray(container) && container.indexOf(item) === -1) {
+      let index = 0;
       let length = container.get('length');
-      for (let i = 0; i < length; i++) {
-        let controls = container.objectAt(i).get('controls');
-        let length = controls.get('length');
-        for (let i = 0; i < length; i++) {
-          let control = controls.objectAt(i);
-          if (control instanceof FdEditformGroup) {
-            _container = this._findRowContainer(row, control.get('rows'));
-          }
-
-          if (control instanceof FdEditformTabgroup) {
-            let tabs = control.get('tabs');
-            let length = tabs.get('length');
-            for (let i = 0; i < length; i++) {
-              _container = this._findRowContainer(row, tabs.objectAt(i).get('rows'));
-              if (_container) {
-                break;
-              }
-            }
-          }
-
-          if (_container) {
-            break;
-          }
-        }
-
-        if (_container) {
-          break;
-        }
+      while (index < length && !foundContainer) {
+        foundContainer = this._findItemContainer(item, container.objectAt(index++));
       }
     } else {
-      _container = container;
+      foundContainer = container;
     }
 
-    return _container;
-  },
-
-  /**
-    Looks for a container that contains the control.
-
-    @method _findControlContainer
-    @param {FdEditformControl} control The sought control.
-    @param {Ember.Array} container The root container.
-    @return {Ember.Array} The container that was found or `null`.
-  */
-  _findControlContainer(control, container) {
-    let _container = null;
-    let length = container.get('length');
-    for (let i = 0; i < length; i++) {
-      let controls = container.objectAt(i).get('controls');
-      if (controls && controls.indexOf(control) === -1) {
-        let length = controls.get('length');
-        for (let i = 0; i < length; i++) {
-          let _control = controls.objectAt(i);
-          if (_control instanceof FdEditformGroup) {
-            _container = this._findControlContainer(control, _control.get('rows'));
-          } else if (_control instanceof FdEditformTabgroup) {
-            let tabs = _control.get('tabs');
-            let length = tabs.get('length');
-            for (let i = 0; i < length; i++) {
-              _container = this._findControlContainer(control, tabs.objectAt(i).get('rows'));
-              if (_container) {
-                break;
-              }
-            }
-          }
-
-          if (_container) {
-            break;
-          }
-        }
-      } else if (controls) {
-        _container = controls;
-      }
-
-      if (_container) {
-        break;
-      }
-    }
-
-    return _container;
+    return foundContainer;
   },
 });
