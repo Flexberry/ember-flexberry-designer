bc10fb379f47a5612f9ea9ed5045d39af93fee32
kpepelyaeva <pepelyaevaks@gmail.com>
2018-07-02 18:04:26 +0500

    Add common primitives util

---
+++ app/utils/fd-common-primitives.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-flexberry-designer/utils/fd-common-primitives';
---
+++ tests/unit/utils/fd-common-primitives-test.js
@@ -0,0 +1,10 @@
+import fdCommonPrimitives from 'dummy/utils/fd-common-primitives';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | fd common primitives');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = fdCommonPrimitives();
+  assert.ok(true);
+});
