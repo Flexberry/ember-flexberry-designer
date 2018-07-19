1d5edd85ab9b8c7c04644c038c60d4ac80bf510f
Eugene Kolegov <ekolegov@outlook.com>
2018-07-19 15:49:17 +0500

    Feature fd-diagram-edit-form form component usage update

--- addon/routes/fd-diagram-edit-form.js
+++ addon/routes/fd-diagram-edit-form.js
@@ -5,11 +5,4 @@ export default EditFormRoute.extend({
   modelProjection: 'FdUmlCad',
 
   modelName: 'fd-dev-uml-cad',
-
-  setupController: function (controller, model) {
-    this._super(...arguments);
-    let primitivesJsonString = model.get('primitivesJsonString');
-    model.set('primitives', eval(primitivesJsonString));
-  }
-
 });
