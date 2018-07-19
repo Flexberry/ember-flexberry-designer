137e7bddf7980c74a1bad36b5bc098381bdd73d2
Eugene Kolegov <ekolegov@outlook.com>
2018-07-19 12:51:30 +0500

    Feature fd-uml-[association|composition|generalization] objects definition

---
+++ addon/objects/uml-primitives/fd-uml-association.js
@@ -0,0 +1,55 @@
+/**
+  @module ember-flexberry-designer
+*/
+
+import Ember from 'ember';
+import joint from 'npm:jointjs';
+
+import FdUmlLink from './fd-uml-link';
+
+/**
+  An object that describes a link of the association type on the UML diagram.
+
+  @class FdUmlAssociation
+  @extends FdUmlLink
+*/
+export default FdUmlLink.extend({
+  /**
+    The start multiplicity of a link.
+
+    @property startMultiplicity
+    @type String
+  */
+  startMultiplicity: Ember.computed.alias('primitive.StartMultTxt.Text'),
+
+  /**
+    The end multiplicity of a link.
+
+    @property endMultiplicity
+    @type String
+  */
+  endMultiplicity: Ember.computed.alias('primitive.EndMultTxt.Text'),
+
+  /**
+    The array of labels for this link.
+
+    @property labels
+    @type Array
+  */
+  labels: Ember.computed('startMultiplicity', 'endMultiplicity', function() {
+    return [
+      { attrs: { text: { text: this.get('startMultiplicity') } } },
+      { attrs: { text: { text: this.get('endMultiplicity') } } },
+    ];
+  }),
+
+  /**
+    See {{#crossLink "FdUmlPrimitive/JointJS:method"}}here{{/crossLink}}.
+
+    @method JointJS
+  */
+  JointJS() {
+    let properties = this.getProperties('id', 'source', 'target', 'vertices', 'labels');
+    return new joint.shapes.flexberryUml.Association(properties);
+  },
+});
