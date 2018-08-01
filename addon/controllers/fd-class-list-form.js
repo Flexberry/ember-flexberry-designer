44613bb444905b4e747b271383b8744a8abba734
Semyon Volkov <volkovsema@gmail.com>
2018-08-01 15:50:59 +0500

    Work in progress

--- addon/controllers/fd-class-list-form.js
+++ addon/controllers/fd-class-list-form.js
@@ -8,5 +8,33 @@ export default ListFormController.extend({
     @type String
     @default 'fd-class-edit-form'
    */
-  editFormRoute: 'fd-class-edit-form'
+  editFormRoute: 'fd-class-edit-form',
+
+  actions: {
+    onRowClick(record, options) {
+      let editFormRoute;
+      switch (record.data.stereotype) {
+        case '«listform»':
+          editFormRoute = 'fd-listform-constructor';
+        break;
+        case '«editform»':
+          editFormRoute = 'fd-editform-constructor';
+        break;
+        default:
+          editFormRoute = this.get('editFormRoute');
+        break;
+      }
+
+      Ember.assert('Edit form route must be defined for flexberry-objectlistview', editFormRoute);
+      if (Ember.isNone(options)) {
+        options = {};
+        options.editFormRoute = editFormRoute;
+      } else {
+        options = Ember.merge(options, { editFormRoute: editFormRoute });
+      }
+
+      this.transitionToRoute(editFormRoute, record);
+    }
+
+  }
 });
