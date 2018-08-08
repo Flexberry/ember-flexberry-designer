3dc76bf10144ff29de9a1f55490dddb0a27799a6
Flexberry-man <mail@flexberry.net>
2018-08-08 06:10:32 +0000

    Update gh-pages for develop branch

--- dummy/develop/assets/tests.js
+++ dummy/develop/assets/tests.js
@@ -7315,7 +7315,7 @@ define('dummy/tests/unit/transforms/containers-tree-test', ['exports', 'ember', 
       caption: 'Настройка рассылки писем',
       text: 'Настройка рассылки писем',
       type: 'property',
-      id: 'p0l0i2',
+      id: 'p2l0i2',
       url: ''
     })]);
 
@@ -7325,7 +7325,7 @@ define('dummy/tests/unit/transforms/containers-tree-test', ['exports', 'ember', 
       caption: 'Журнал импорта',
       text: 'Журнал импорта',
       type: 'property',
-      id: 'p0l0i3',
+      id: 'p2l0i3',
       url: ''
     })]);
 
@@ -7355,7 +7355,7 @@ define('dummy/tests/unit/transforms/containers-tree-test', ['exports', 'ember', 
       caption: 'Учет рабочего времени',
       text: 'Учет рабочего времени',
       type: 'property',
-      id: 'p0l0i4',
+      id: 'p1l0i4',
       url: ''
     }), _emberFlexberryDesignerObjectsFdAppstructTree['default'].create({
       className: 'ПроизводственныйКалендарьWebL',
