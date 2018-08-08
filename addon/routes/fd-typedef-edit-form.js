3fedf37522248d9e04589da841ef972a2703c72a
Semyon Volkov <svolkov_1@edu.hse.ru>
2018-08-08 10:59:41 +0500

    Add getting typemap

--- addon/routes/fd-typedef-edit-form.js
+++ addon/routes/fd-typedef-edit-form.js
@@ -4,6 +4,7 @@ export default Ember.Route.extend({
   modelProjection: 'TypeDefinitionE',
   modelName: 'fd-dev-type-definition',
   currentProjectContext: Ember.inject.service('fd-current-project-context'),
+  thisTypedef: undefined,
 
   afterModel: function(model) {
     this._super(model);
@@ -14,16 +15,21 @@ export default Ember.Route.extend({
   },
 
   model: function(params) {
-
     let store = this.get('store');
     let stageId = this.get('currentProjectContext').getCurrentStage();
 
     let allClasses = store.peekAll('fd-dev-class');
     let allStages = store.peekAll('fd-dev-stage');
 
-    let typedef = allClasses.findBy('id', params.id);
+    this.set('thisTypedef', allClasses.findBy('id', params.id));
+    let thisTypedef = this.get('thisTypedef').get('name');
+
 
     let stage = allStages.findBy('id', stageId); // typemaps are inside, in XML. Need to find one equal to current type and let change it.
-    return modelHash;
+    let typeMapCSStr = stage.get('typeMapCSStr').findBy('name', thisTypedef);
+    let typeMapOracleStr = stage.get('typeMapOracleStr');
+    let typeMapPostgreStr = stage.get('typeMapPostgreStr');
+    let typeMapSQLStr = stage.get('typeMapSQLStr');
+    return azaza;
   }
 });
