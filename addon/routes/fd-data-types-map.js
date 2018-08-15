671cc0d0b013ca2babdaaf3784a0b558b45e62c2
Eugene Kolegov <ekolegov@outlook.com>
2018-08-15 11:27:21 +0500

    Feature fd-data-types-map form not empty implementation

--- addon/routes/fd-data-types-map.js
+++ addon/routes/fd-data-types-map.js
@@ -1,4 +1,71 @@
+/**
+  @module ember-flexberry-designer
+*/
+
 import Ember from 'ember';
 
+/**
+  Route for the edit form of the type map.
+
+  @class FdDataTypesMapRoute
+  @extends <a href="http://emberjs.com/api/classes/Ember.Route.html">Ember.Route</a>
+*/
 export default Ember.Route.extend({
+  /**
+    Link to {{#crossLink "FdCurrentProjectContextService"}}FdCurrentProjectContextService{{/crossLink}}.
+
+    @property currentContext
+    @type FdCurrentProjectContextService
+  */
+  currentContext: Ember.inject.service('fd-current-project-context'),
+
+  actions: {
+    /**
+      See [EmberJS API](https://emberjs.com/).
+
+      @method didTransition
+    */
+    didTransition() {
+      // Save the type map if it has just been generated with default values.
+      if (this.get('controller').serializeTypeMap()) {
+        this.get('controller').send('save');
+      }
+    },
+
+    /**
+      See [EmberJS API](https://emberjs.com/).
+
+      @method willTransition
+    */
+    willTransition(transition) {
+      let controller = this.get('controller');
+      if (controller.serializeTypeMap()) {
+        this.send('showModalDialog', 'modal/save', { controller });
+        transition.abort();
+      }
+    },
+  },
+
+  /**
+    See [EmberJS API](https://emberjs.com/).
+
+    @method model
+  */
+  model() {
+    return {
+      stage: this.get('currentContext').getCurrentStageModel(),
+      classes: this.get('store').peekAll('fd-dev-class'),
+    };
+  },
+
+  /**
+    See [EmberJS API](https://emberjs.com/).
+
+    @method model
+  */
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      controller.set('showTypeMap', true);
+    }
+  },
 });
