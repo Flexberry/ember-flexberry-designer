87222853dd6eca34decd0678a463fd2c379415f3
Semyon Volkov <volkovsema@gmail.com>
2018-09-10 12:42:37 +0500

    Fix layers

--- addon/objects/uml-primitives/fd-uml-partition.js
+++ addon/objects/uml-primitives/fd-uml-partition.js
@@ -3,7 +3,6 @@
 */
 
 import Ember from 'ember';
-import joint from 'npm:jointjs';
 
 import FdUmlElement from './fd-uml-element';
 import { BaseObject } from './fd-uml-object';
@@ -45,7 +44,21 @@ export default FdUmlElement.extend({
   @constructor
 */
 export let Partition = BaseObject.define('flexberry.uml.Partition', {
-  attrs: { '.flexberry-uml-header-text': { 'font-weight':'bold', 'ref-y': 0, 'y-alignment': 'start' } },
+  attrs: {
+    '.flexberry-uml-header-text':
+    {
+      'font-weight': 'bold',
+      'ref-y': 0,
+      'y-alignment': 'start'
+    },
+    '.flexberry-uml-header-rect':
+    {
+      'stroke': 'black',
+      'stroke-width': 1,
+      'fill': '#ffffff',
+      'fill-opacity': '0.0'
+    },
+  },
   heightPadding: 60
 }, {
   markup: [
@@ -55,5 +68,11 @@ export let Partition = BaseObject.define('flexberry.uml.Partition', {
     '</g>',
     '<text class="flexberry-uml-header-text"/>',
     '</g>'
-  ].join('')
+  ].join(''),
+  initialize: function () {
+    BaseObject.prototype.initialize.apply(this, arguments);
+    this.on('change', function() {
+      this.toBack({ deep: true });
+    });
+  },
 });
