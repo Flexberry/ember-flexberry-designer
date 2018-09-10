deeb0f4221a880678a652d85f055a929fb700d31
Sokolov Andrey <s.andrey-2011@yandex.ru>
2018-09-10 13:46:50 +0500

    Fix travis

--- addon/objects/uml-primitives/fd-uml-baselink.js
+++ addon/objects/uml-primitives/fd-uml-baselink.js
@@ -171,8 +171,8 @@ export let Link = joint.dia.Link.define('flexberry.uml.Link', {
 
 export let BaseLinkWithUnderline = Link.define('flexberry.uml.BaseLinkWithUnderline', {
 }, {
-    initialize: function() {
-      this.label(2, { attrs: { text: { 'text-decoration': 'underline', } } });
-      joint.dia.Link.prototype.initialize.apply(this, arguments);
-    },
+  initialize: function() {
+    this.label(2, { attrs: { text: { 'text-decoration': 'underline', } } });
+    joint.dia.Link.prototype.initialize.apply(this, arguments);
+  }
 });
