da2f090bd122db1af409757381482fcc2f27d504
Sokolov Andrey <s.andrey-2011@yandex.ru>
2018-09-19 11:37:56 +0500

    Fix merge

--- addon/controllers/fd-diagram-edit-form.js
+++ addon/controllers/fd-diagram-edit-form.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 import EditFormController from 'ember-flexberry/controllers/edit-form';
 import FdWorkPanelToggler from '../mixins/fd-work-panel-toggler';
+import FdFormUnsavedData from '../mixins/fd-form-unsaved-data';
 import FdAcrionsForCadPrimitivesMixin from '../mixins/actions-for-primitives/fd-actions-for-cad-primitives';
 import FdAcrionsForCommonPrimitivesMixin from '../mixins/actions-for-primitives/fd-actions-for-common-primitives';
 
 export default EditFormController.extend(
 FdWorkPanelToggler,
+FdFormUnsavedData,
 FdAcrionsForCadPrimitivesMixin,
 FdAcrionsForCommonPrimitivesMixin, {
   parentRoute: 'fd-diagram-list-form',
