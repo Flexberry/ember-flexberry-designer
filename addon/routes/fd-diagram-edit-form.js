5e1241cb8325b1dc4f296290b4eeba3a2c6dc78d
Sokolov Andrey <s.andrey-2011@yandex.ru>
2018-09-25 17:31:02 +0500

    Olv for all diagrams

--- addon/routes/fd-diagram-edit-form.js
+++ addon/routes/fd-diagram-edit-form.js
@@ -3,7 +3,7 @@ import FdFormCheckTransitionMixin from '../mixins/fd-form-check-transition';
 
 export default EditFormRoute.extend(FdFormCheckTransitionMixin, {
 
-  modelProjection: 'FdDiagramE',
+  modelProjection: 'FdDiagram',
 
   modelName: 'fd-diagram'
 });
