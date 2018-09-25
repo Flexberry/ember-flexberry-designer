8f4c86168ffffee04b1adc0a4482dfe7deaf8fb9
Sokolov Andrey <s.andrey-2011@yandex.ru>
2018-09-25 16:04:02 +0500

    Add button for creating diagrams

--- addon/routes/fd-diagram-edit-form/new.js
+++ addon/routes/fd-diagram-edit-form/new.js
@@ -4,4 +4,15 @@ export default EditFormNewRoute.extend({
   modelProjection: 'EditFormView',
   modelName: 'fd-dev-uml-cad',
   templateName: 'fd-diagram-edit-form',
+
+  /**
+    See [EmberJS API](https://emberjs.com/).
+
+    @method model
+  */
+  model() {
+    let newRecord = this._super(...arguments);
+    newRecord.set('primitivesJsonString', '[]');
+    return newRecord;
+  }
 });
