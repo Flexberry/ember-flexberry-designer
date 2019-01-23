b2d88bdab4f4768f2ceb4c90b23b98230c2cbdd7
kpepelyaeva <pepelyaevaks@gmail.com>
2019-01-23 18:05:02 +0500

    Fix JSHint error

--- addon/mixins/fd-sheet-mixin.js
+++ addon/mixins/fd-sheet-mixin.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 
-  commonVisible : {
-    "visible": ""
+  commonVisible: {
+    'visible': ''
   },
 
   actions: {
     openSheet() {
-      let visible =  this.get('commonVisible').visible === '' ? 'visible': '';
+      let visible =  this.get('commonVisible').visible === '' ? 'visible' : '';
       this.set('commonVisible.visible', visible);
       Ember.$('.pushable').addClass('fade');
     }
