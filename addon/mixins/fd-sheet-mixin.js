bb33929871ea453f0e10d70c31474816db83be4e
kpepelyaeva <pepelyaevaks@gmail.com>
2019-01-30 14:43:42 +0500

    feat(components): Add style for fd-list component
    METADATA: Resolve #223

--- addon/mixins/fd-sheet-mixin.js
+++ addon/mixins/fd-sheet-mixin.js
@@ -8,8 +8,7 @@ export default Ember.Mixin.create({
 
   actions: {
     openSheet() {
-      let visible =  this.get('commonVisible').visible === '' ? 'visible' : '';
-      this.set('commonVisible.visible', visible);
+      this.set('commonVisible.visible', 'visible');
       Ember.$('.pushable').addClass('fade');
     }
   }
