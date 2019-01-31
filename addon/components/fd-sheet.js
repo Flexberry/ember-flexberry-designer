ee251082e076e5a72120948cd9c8bbf6c9b88162
kpepelyaeva <pepelyaevaks@gmail.com>
2019-01-31 17:10:43 +0500

    fix: Fix fd-sheet content animate (test version)
    METADATA: Resolve #220

--- addon/components/fd-sheet.js
+++ addon/components/fd-sheet.js
@@ -10,14 +10,23 @@ export default Ember.Component.extend(fdSheetMixin, {
   expand: false,
 
   actions: {
-    close() {
+    closeSheet() {
       this.set('commonVisible.visible', '');
       this.set('expand', false);
       Ember.$('.pushable').removeClass('fade');
     },
 
     expand() {
-      this.toggleProperty('expand');
+      let contentWidth = this.expand ? '50%' : 'calc(100% - 300px)';
+      Ember.$('.content-mini').css({ opacity: 0.2 });
+      var self = this;
+      Ember.run.schedule('afterRender', () => {
+        self.toggleProperty('expand');
+      });
+
+      Ember.run.later(function() {
+        Ember.$('.content-mini').css({ opacity: '', width: contentWidth });
+      }, 600);
     },
 
     save() {
