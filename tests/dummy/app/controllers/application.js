6d34b7cd3dff98b07172f9361bc587fb865f238b
kpepelyaeva <pepelyaevaks@gmail.com>
2019-02-01 13:50:57 +0500

    feat: Add icons for sitemap
    METADATA: Resolve #219

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -31,31 +31,37 @@ export default Ember.Controller.extend(FdWorkPanelToggler, {
         link: 'fd-application-model',
         caption: i18n.t('forms.application.sitemap.root.fd-application-model.caption'),
         title: i18n.t('forms.application.sitemap.root.fd-application-model.title'),
+        icon: 'icon-fd-menu'
       },
       {
         link: 'fd-diagrams',
         caption: i18n.t('forms.application.sitemap.root.fd-diagrams.caption'),
         title: i18n.t('forms.application.sitemap.root.fd-diagrams.title'),
+        icon: 'icon-fd-diagram'
       },
       {
         link: 'fd-navigation',
         caption: i18n.t('forms.application.sitemap.root.fd-navigation.caption'),
         title: i18n.t('forms.application.sitemap.root.fd-navigation.title'),
+        icon: 'icon-fd-sitemap'
       },
       {
         link: 'fd-generation',
         caption: i18n.t('forms.application.sitemap.root.fd-generation.caption'),
         title: i18n.t('forms.application.sitemap.root.fd-generation.title'),
+        icon: 'icon-fd-shipping-box'
       },
       {
         link: 'fd-setting',
         caption: i18n.t('forms.application.sitemap.root.fd-setting.caption'),
         title: i18n.t('forms.application.sitemap.root.fd-setting.title'),
+        icon: 'icon-fd-gear'
       },
       {
         link: 'fd-architecture',
         caption: i18n.t('forms.application.sitemap.root.fd-architecture.caption'),
         title: i18n.t('forms.application.sitemap.root.fd-architecture.title'),
+        icon: 'icon-fd-diagram'
       });
     }
 
@@ -76,21 +82,25 @@ export default Ember.Controller.extend(FdWorkPanelToggler, {
         link: 'fd-all-projects',
         caption: i18n.t('forms.application.sitemap.root.fd-all-projects.caption'),
         title: i18n.t('forms.application.sitemap.root.fd-all-projects.title'),
+        icon: 'icon-fd-view'
       },
       {
         link: '',
         caption: i18n.t('forms.application.sitemap.root.fd-requests.caption'),
         title: i18n.t('forms.application.sitemap.root.fd-requests.title'),
+        icon: 'icon-fd-email'
       },
       {
         link: '',
         caption: i18n.t('forms.application.sitemap.root.fd-docs.caption'),
         title: i18n.t('forms.application.sitemap.root.fd-docs.title'),
+        icon: 'icon-fd-book'
       },
       {
         link: '',
         caption: i18n.t('forms.application.sitemap.root.fd-chat.caption'),
         title: i18n.t('forms.application.sitemap.root.fd-chat.title'),
+        icon: 'icon-fd-speech-bubble'
       });
     }
 
