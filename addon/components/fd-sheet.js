76434c500d07fbd66bdceba9adcd001f71145826
kpepelyaeva <pepelyaevaks@gmail.com>
2019-02-01 13:55:24 +0500

    fix: Update animation expand fd-sheet
    METADATA: Resolve #220

--- addon/components/fd-sheet.js
+++ addon/components/fd-sheet.js
@@ -14,10 +14,14 @@ export default Ember.Component.extend(fdSheetMixin, {
       this.set('commonVisible.visible', '');
       this.set('expand', false);
       Ember.$('.pushable').removeClass('fade');
+      Ember.run.later(function() {
+        Ember.$('.content-mini').css({ width: '' });
+      }, 1000);
     },
 
     expand() {
-      let contentWidth = this.expand ? '50%' : 'calc(100% - 300px)';
+      let sidebarWidth = Ember.$('.ui.sidebar.main.menu').width();
+      let contentWidth = this.expand ? '50%' : 'calc(100% - ' + sidebarWidth + 'px)';
       Ember.$('.content-mini').css({ opacity: 0.2 });
       Ember.run.schedule('afterRender', this, () => {
         this.toggleProperty('expand');
