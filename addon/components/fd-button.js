e9ea233e28205cbb5fdd9dd8e7fb6639601efbb9
kpepelyaeva <pepelyaevaks@gmail.com>
2019-02-05 14:05:31 +0500

    feat(components): Add fd-button component
    METADATA: Resolve #224

---
+++ addon/components/fd-button.js
@@ -0,0 +1,12 @@
+import FlexberryButtonComponent from 'ember-flexberry/components/flexberry-button';
+import layout from '../templates/components/fd-button';
+
+export default FlexberryButtonComponent.extend({
+  layout,
+
+  tagName: 'button',
+
+  iconClass: 'icon-fd-plus-thin',
+
+  classNames: ['basic circular'],
+});
