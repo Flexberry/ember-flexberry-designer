461448cfa2244a1072bb58a913074ac5576503f8
kpepelyaeva <pepelyaevaks@gmail.com>
2019-02-07 14:53:53 +0500

    feat(components): Add sidebar toggler
    METADATA: #256

--- addon/mixins/fd-sheet-mixin.js
+++ addon/mixins/fd-sheet-mixin.js
@@ -10,6 +10,19 @@ export default Ember.Mixin.create({
     openSheet() {
       this.set('commonVisible.visible', 'visible');
       Ember.$('.pushable').addClass('fade');
+
+      let sidebarWidth = Ember.$('.ui.sidebar.main.menu').width();
+
+      let sheetTranslate = `translate3d(calc(50% - ${sidebarWidth}px), 0, 0)`;
+      Ember.$('.fd-sheet').css({ 'transform': sheetTranslate });
+    },
+
+    animatingSheetContent(contentWidth, speed) {
+      Ember.$('.fd-sheet .content-mini').css({ opacity: 0.2 });
+
+      Ember.run.later(function() {
+        Ember.$('.fd-sheet .content-mini').css({ opacity: '', width: contentWidth });
+      }, speed);
     }
   }
 });
