386966aa0d312b5cac0758f39076eed53ef4931b
Eugene Kolegov <ekolegov@outlook.com>
2019-03-03 20:21:25 +0500

    feat: Add project selection in project list

--- addon/controllers/fd-all-projects/index.js
+++ addon/controllers/fd-all-projects/index.js
@@ -21,6 +21,14 @@ export default Ember.Controller.extend({
   active: false,
 
   /**
+    Link to {{#crossLink "FdCurrentProjectContextService"}}{{/crossLink}}.
+
+    @property projectContext
+    @type FdCurrentProjectContextService
+  */
+  projectContext: Ember.inject.service('fd-current-project-context'),
+
+  /**
     Request to search for projects.
 
     @property search
@@ -35,4 +43,28 @@ export default Ember.Controller.extend({
     @property queryParams
   */
   queryParams: ['search'],
+
+  actions: {
+    /**
+      Choose a project for further work with it.
+
+      @method actions.chooseProject
+      @param {DS.Model} project The selected project.
+    */
+    chooseProject(project) {
+      this.get('projectContext').setCurrentConfiguration(project.get('configuration'));
+      this.get('projectContext').setCurrentStage(project);
+      this.transitionToRoute('fd-application-model');
+    },
+
+    /**
+      Pin the selected project.
+
+      @method actions.pinProject
+      @param {DS.Model} project The selected project.
+    */
+    pinProject(/* project */) {
+      throw new Error('Not implemented.');
+    },
+  },
 });
