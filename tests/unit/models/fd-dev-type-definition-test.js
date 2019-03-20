3af245e2eccb53fa4587bdb83046d49344f9e6ce
leoleopon <safari2002@list.ru>
2019-03-20 18:29:17 +0500

    Fix needs for 30 unit tests

--- tests/unit/models/fd-dev-type-definition-test.js
+++ tests/unit/models/fd-dev-type-definition-test.js
@@ -74,7 +74,8 @@ moduleForModel('fd-dev-type-definition', 'Unit | Model | fd-dev-type-definition'
     'model:fd-user-auth-profile',
     'model:fd-user-in-stage',
     'model:fd-view',
-    'model:i-c-s-soft-s-t-o-r-m-n-e-t-security-agent'
+    'model:i-c-s-soft-s-t-o-r-m-n-e-t-security-agent',
+    'service:syncer'
   ]
 });
 
