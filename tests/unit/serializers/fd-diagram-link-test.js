ea453645d61284718c8a4c0ed733b7baa93c97dd
leoleopon <safari2002@list.ru>
2019-03-22 11:18:05 +0500

    Fix needs for 31 unit tests

--- tests/unit/serializers/fd-diagram-link-test.js
+++ tests/unit/serializers/fd-diagram-link-test.js
@@ -89,7 +89,9 @@ moduleForModel('fd-diagram-link', 'Unit | Serializer | fd-diagram-link', {
     'model:fd-user-auth-profile',
     'model:fd-user-in-stage',
     'model:fd-view',
-    'model:i-c-s-soft-s-t-o-r-m-n-e-t-security-agent'
+    'model:i-c-s-soft-s-t-o-r-m-n-e-t-security-agent',
+
+    'service:syncer'
   ]
 });
 
--- tests/unit/serializers/fd-plugin-on-rep-object-test.js
+++ tests/unit/serializers/fd-plugin-on-rep-object-test.js
@@ -89,7 +89,9 @@ moduleForModel('fd-plugin-on-rep-object', 'Unit | Serializer | fd-plugin-on-rep-
     'model:fd-user-auth-profile',
     'model:fd-user-in-stage',
     'model:fd-view',
-    'model:i-c-s-soft-s-t-o-r-m-n-e-t-security-agent'
+    'model:i-c-s-soft-s-t-o-r-m-n-e-t-security-agent',
+
+    'service:syncer'
   ]
 });
 
