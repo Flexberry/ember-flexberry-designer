ac351e4ba7050410f9e53a4205ab2119eeac3836
OPARINA Olga <ooparina@ics.perm.ru>
2019-03-26 18:10:07 +0500

    feat add round button component

---
+++ addon/components/fd-round-button.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import FlexberryButtonComponent from 'ember-flexberry/components/flexberry-button';
+import layout from '../templates/components/fd-round-button';
+
+export default FlexberryButtonComponent.extend({
+  layout,
+
+  tagName: 'div',
+
+  iconClass: 'icon-fd-plus-thin',
+
+  classNames: ['fd-round-button'],
+
+  colorClass: 'blue',
+
+  init() {
+    this._super(...arguments);
+    this.classNames.push(this.get('colorClass'));
+  }
+});
---
+++ tests/integration/components/fd-round-button-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('fd-round-button', 'Integration | Component | fd round button', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{fd-round-button}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#fd-round-button}}
+      template block text
+    {{/fd-round-button}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
