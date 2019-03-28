9aa9a39bc859f1091480ac9e88ad27f311f3c983
Sokolov Andrey <s.andrey-2011@yandex.ru>
2019-03-28 15:13:42 +0500

    View logic moved in class edit panel

--- addon/components/fd-editing-panels/fd-implementation-editing-panel.js
+++ addon/components/fd-editing-panels/fd-implementation-editing-panel.js
@@ -2,14 +2,12 @@ import Ember from 'ember';
 import FdUpdateBsValueMixin from '../../mixins/fd-editing-panels/fd-update-bs-value';
 import FdUpdateAttributeValueMixin from '../../mixins/fd-editing-panels/fd-update-attribute-value';
 import FdUpdateMethodeValueMixin from '../../mixins/fd-editing-panels/fd-update-method-value';
-import FdUpdateViewValueMixin from '../../mixins/fd-editing-panels/fd-update-view-value';
 import layout from '../../templates/components/fd-editing-panels/fd-implementation-editing-panel';
 
 export default Ember.Component.extend(
   FdUpdateBsValueMixin,
   FdUpdateAttributeValueMixin,
-  FdUpdateMethodeValueMixin,
-  FdUpdateViewValueMixin, {
+  FdUpdateMethodeValueMixin, {
   layout,
 
   /**
@@ -20,4 +18,71 @@ export default Ember.Component.extend(
     @default undefined
   */
   model: undefined,
+
+  /**
+    Service for managing the state of the sheet component.
+
+    @property fdSheetService
+    @type FdSheetService
+  */
+  fdSheetService: Ember.inject.service(),
+
+  /**
+    Table headers for view.
+
+    @property tableViewForView
+    @type Array
+  */
+  tableViewForView: [
+    {
+      columnCaption: 'components.fd-attribute-table.view.name',
+      columnProperty: 'name',
+      attrPlaceholder: 'components.fd-attribute-table.view.name-placeholder',
+      columnClass: 'three'
+    },
+    {
+      columnCaption: 'components.fd-attribute-table.view.description',
+      columnProperty: 'description',
+      attrPlaceholder: 'components.fd-attribute-table.view.description-placeholder',
+      columnClass: 'four'
+    },
+  ],
+
+  /**
+    Button locale path for view.
+
+    @property viewButton
+    @type Object
+  */
+  viewButton: {
+    createBtn: 'components.fd-attribute-table.view.create-btn',
+    deleteBtn: 'components.fd-attribute-table.view.delete-btn',
+  },
+
+  actions: {
+
+    /**
+      Method create view from table.
+
+      @method actions.createView
+    */
+    createView() {
+      let store = this.get('store');
+      let model = this.get('model');
+      let view = store.createRecord('fd-dev-view', {
+        class: model
+      });
+
+      this.send('openViewSheet', view);
+    },
+
+    /**
+      Method open view sheet.
+
+      @method actions.openViewSheet
+    */
+    openViewSheet(selectView) {
+      this.sendAction('openViewSheetController', selectView);
+    }
+  }
 });
