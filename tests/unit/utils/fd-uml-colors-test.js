2db78792bbad0766fc21fce320cec4e925b676cc
kafnevod <kaf@ics.perm.ru>
2019-05-16 15:02:35 +0500

    Debug

--- tests/unit/utils/fd-uml-colors-test.js
+++ tests/unit/utils/fd-uml-colors-test.js
@@ -1,7 +1,7 @@
 import fdUmlColors from 'dummy/utils/fd-uml-colors';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | fd-uml-colors', function(hooks) {
+module('Unit | Utility | fd-uml-colors', function() {
 
   // Replace this with your real tests.
   test('it works', function(assert) {
