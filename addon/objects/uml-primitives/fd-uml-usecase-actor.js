b89b60b2599a6a1a3ee6e7870e13c874f43c2553
kafnevod <kaf@ics.perm.ru>
2019-06-24 10:12:34 +0500

    Add inputs to usecase actor

--- addon/objects/uml-primitives/fd-uml-usecase-actor.js
+++ addon/objects/uml-primitives/fd-uml-usecase-actor.js
@@ -7,6 +7,7 @@ import { isArray } from '@ember/array';
 
 import FdUmlElement from './fd-uml-element';
 import { BaseObject } from './fd-uml-baseobject';
+import joint from 'npm:jointjs';
 
 /**
   An object that describes a UsecaseActor on the UML diagram.
@@ -88,7 +89,15 @@ export let UsecaseActor = BaseObject.define('flexberry.uml.UsecaseActor', {
     '</g>'
   ].join(''),
 
-  updateRectangles: function () {
-    this.updateRectanglesOld();
+  initialize: function () {
+    BaseObject.prototype.initialize.apply(this, arguments);
+
   }
+
+});
+
+joint.shapes.flexberry.uml.UsecaseActorView = joint.shapes.flexberry.uml.BaseObjectView.extend({
+  updateRectangles() {
+    joint.shapes.flexberry.uml.BaseObjectView.prototype.updateRectangles.apply(this, arguments);
+  },
 });
