4cc979c08973eefe7b6d65454e793d009a9ce917
AntonIV <anton-iv87@yandex.ru>
2019-08-09 14:10:27 +0500

    Fix input with/height calculate in signals

--- addon/objects/uml-primitives/fd-uml-signal-receipt.js
+++ addon/objects/uml-primitives/fd-uml-signal-receipt.js
@@ -81,7 +81,7 @@ export let SignalReceiptRight = BaseObject.define('flexberry.uml.SignalReceiptRi
 }, {
   markup: [
     '<g class="rotatable">',
-    '<path class="flexberry-uml-header-rect flexberry-uml-header-rect-path"/>',
+    '<path class="flexberry-uml-header-rect-path"/>',
     '</g>'
   ].join('')
 });
@@ -102,10 +102,15 @@ joint.shapes.flexberry.uml.SignalReceiptRightView = joint.shapes.flexberry.uml.B
     let newWidth = 0;
     rects.forEach(function (rect) {
       if (this.markup.includes('flexberry-uml-' + rect.type + '-rect') && rect.element.inputElements) {
-        let $buffer = rect.element.inputElements.find('.input-buffer');
         let rectHeight = 0;
         let inputs = rect.element.inputElements.find('.signal-input');
-        inputs.each(function () {
+        let inputsDiv = inputs[0].parentElement;
+        if (! inputsDiv.parentElement || ! inputsDiv.parentElement.className.includes('joint-paper')) {
+          let jointPaper = $('.joint-paper')[0];
+          jointPaper.appendChild(inputsDiv);
+        }
+        let $buffer = rect.element.inputElements.find('.input-buffer');
+        inputs.each(function() {
           let $input = $(this);
           $buffer.css('font-weight', $input.css('font-weight'));
           $buffer.text($input.val());
@@ -133,7 +138,6 @@ joint.shapes.flexberry.uml.SignalReceiptRightView = joint.shapes.flexberry.uml.B
       rect.element.attr('.flexberry-uml-' + rect.type + '-rect/width', newWidth);
     });
 
-    //Resize signal shape path
     rects.forEach((rect) => {
       let points = this.recalculatePathPoints(newWidth, newHeight);
       //set path
@@ -149,17 +153,17 @@ joint.shapes.flexberry.uml.SignalReceiptRightView = joint.shapes.flexberry.uml.B
   },
 
   recalculatePathPoints(newWidth, newHeight) {
-      //increase rectangle width, because it have corner inside
-      let newWidthInc = newWidth + 20;
-      //calculating path points
-      let points = A();
-      points[0] = '0 0';
-      points[1] = newWidthInc.toString() + ' 0';
-      points[2] = (newWidthInc - 20).toString() + ' ' + (newHeight / 2).toString();
-      points[3] = newWidthInc.toString() + ' ' + newHeight.toString();
-      points[4] = '0 ' + newHeight.toString();
+    //increase rectangle width, because it have corner inside
+    let newWidthInc = newWidth + 20;
+    //calculating path points
+    let points = A();
+    points[0] = '0 0';
+    points[1] = newWidthInc.toString() + ' 0';
+    points[2] = (newWidthInc - 20).toString() + ' ' + (newHeight / 2).toString();
+    points[3] = newWidthInc.toString() + ' ' + newHeight.toString();
+    points[4] = '0 ' + newHeight.toString();
 
-      return points;
+    return points;
   }
 });
 
@@ -184,16 +188,16 @@ export let SignalReceiptLeft = SignalReceiptRight.define('flexberry.uml.SignalRe
 
 joint.shapes.flexberry.uml.SignalReceiptLeftView = joint.shapes.flexberry.uml.SignalReceiptRightView .extend({
    recalculatePathPoints(newWidth, newHeight) {
-      //increase rectangle width, because it have corner inside
-      //calculating path points
-      let points = A();
-      points[0] = '-20 0';
-      points[1] = newWidth.toString() + ' 0';
-      points[2] = newWidth.toString() + ' ' + newHeight.toString();
-      points[3] = '-20 ' + newHeight.toString();
-      points[4] = '0 ' + (newHeight / 2).toString();
+    //increase rectangle width, because it have corner inside
+    //calculating path points
+    let points = A();
+    points[0] = '-20 0';
+    points[1] = newWidth.toString() + ' 0';
+    points[2] = newWidth.toString() + ' ' + newHeight.toString();
+    points[3] = '-20 ' + newHeight.toString();
+    points[4] = '0 ' + (newHeight / 2).toString();
 
-      return points;
+    return points;
   }
 });
 
