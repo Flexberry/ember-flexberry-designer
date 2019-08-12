6f70f91e7647aa667c5930004db8a2eec477a269
s-andrey <s.andrey-2011@yandex.ru>
2019-08-12 11:50:05 +0500

    Feature add view edit form (#431)

--- addon/locales/en/components/fd-create-view.js
+++ addon/locales/en/components/fd-create-view.js
@@ -5,44 +5,21 @@ export default {
   'name-caption': 'Name view',
   'description-caption': 'Description view',
   'settings-view': {
-    caption: 'Settings view',
     'name-caption': 'Caption ("~" - empty value)',
-    path: 'Path ("|" tab, "-" group, "\\" separation, eg "|Description\\-Cost")',
+    path: 'Path',
+    'path-placeholder': 'Enter path ("|" tab, "-" group, "\\" separation, eg "|Description\\-Cost")',
     'create-caption': 'Create',
-    visibility: 'Visibility'
   },
   'settings-master': {
     'lookup-type': 'Lookup type',
     'master-property': 'Master property name',
-    'sample-setting': 'Master customization string'
+    'master-property-placeholder': 'Enter master property name',
+    'sample-setting': 'Master customization string',
+    'sample-setting-placeholder': 'Enter master customization string',
   },
   'settings-detail': {
     view: 'View',
     'load-on-load-agregator': 'Load on load agregator',
     load: 'Loading'
   },
-  'table-column': {
-    type: 'type',
-    'view-attributes': 'property',
-    visibility: 'visibility'
-  },
-  'attributes-tree': {
-    caption: 'All attributes',
-    search: 'Enter name to search',
-  },
-  'attributes-panel': {
-    'move-right-btn-title':'Add attribute',
-    'move-left-btn-title':'Remove attribute',
-    'move-up-btn-title':'Move higher',
-    'move-down-btn-title':'Move below',
-    'close-panel-btn-caption':'Click to hide all attributes',
-    'show-panel-btn-caption':'Click to show all attributes',
-  },
-  'form-config-panel': {
-    caption: 'View editor',
-    'view-properties-header': 'View properties',
-    'attribute-properties-header': 'Attribute properties',
-    'attribute-properties-null': 'View attribute is not selected',
-    'attribute-properties-change': 'Click on the right table item to select the view attribute',
-  }
 };
