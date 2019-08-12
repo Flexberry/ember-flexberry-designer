2be960880189178c3ea3ebbc9081d7ddf6da645c
Flexberry-man <mail@flexberry.net>
2019-08-12 06:58:54 +0000

    Update gh-pages for develop branch

--- dummy/develop/assets/tests.js
+++ dummy/develop/assets/tests.js
@@ -2220,11 +2220,6 @@ define('dummy/tests/tests.lint-test', [], function () {
     assert.ok(true, 'unit/services/fd-sheet-service-test.js should pass ESLint\n\n');
   });
 
-  QUnit.test('unit/transforms/fd-definition-test.js', function (assert) {
-    assert.expect(1);
-    assert.ok(true, 'unit/transforms/fd-definition-test.js should pass ESLint\n\n');
-  });
-
   QUnit.test('unit/transforms/new-platform-flexberry-web-designer-business-server-class-test.js', function (assert) {
     assert.expect(1);
     assert.ok(true, 'unit/transforms/new-platform-flexberry-web-designer-business-server-class-test.js should pass ESLint\n\n');
@@ -2253,6 +2248,11 @@ define('dummy/tests/tests.lint-test', [], function () {
   QUnit.test('unit/utils/fd-containers-tree-test.js', function (assert) {
     assert.expect(1);
     assert.ok(true, 'unit/utils/fd-containers-tree-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/utils/fd-definition-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/utils/fd-definition-test.js should pass ESLint\n\n');
   });
 
   QUnit.test('unit/utils/fd-deployment-diagram-primitives-test.js', function (assert) {
@@ -5502,7 +5502,7 @@ define('dummy/tests/unit/serializers/fd-dev-view-test', ['ember-qunit'], functio
 
   (0, _emberQunit.moduleForModel)('fd-dev-view', 'Unit | Serializer | fd-dev-view', {
     // Specify the other units that are required for this test.
-    needs: ['serializer:fd-dev-view', 'transform:file', 'transform:decimal', 'transform:guid', 'transform:fd-definition', 'transform:i-c-s-soft-s-t-o-r-m-n-e-t-access-type', 'transform:i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-t-write-mode', 'transform:i-c-s-soft-s-t-o-r-m-n-e-t-business-data-service-object-events', 'transform:new-platform-flexberry-web-designer-access-mode', 'transform:new-platform-flexberry-web-designer-generation-state', 'transform:s-t-o-r-m-c-a-s-e-repository-access-modifier', 'transform:s-t-o-r-m-c-a-s-e-s-t-o-r-m-n-e-t-repository-parameter-modifier', 'transform:new-platform-flexberry-web-designer-business-server-class', 'model:fd-ad', 'model:fd-aggregation', 'model:fd-application-user', 'model:fd-association', 'model:fd-auth-type', 'model:fd-base-association', 'model:fd-cad', 'model:fd-case-property', 'model:fd-class-storage-type', 'model:fd-class', 'model:fd-cod', 'model:fd-configuration', 'model:fd-dev-aggregation', 'model:fd-dev-associated-detail-view', 'model:fd-dev-association', 'model:fd-dev-attribute', 'model:fd-dev-base-association', 'model:fd-dev-class', 'model:fd-dev-control-type', 'model:fd-dev-diagram-link', 'model:fd-dev-filelink', 'model:fd-dev-form-control', 'model:fd-dev-form-view', 'model:fd-dev-inheritance', 'model:fd-dev-method', 'model:fd-dev-module-setting-type', 'model:fd-dev-module-setting', 'model:fd-dev-parameter', 'model:fd-dev-process-status', 'model:fd-dev-stage-history', 'model:fd-dev-stage', 'model:fd-dev-system', 'model:fd-dev-task', 'model:fd-dev-type-definition', 'model:fd-dev-uml-ad', 'model:fd-dev-uml-cad', 'model:fd-dev-uml-cod', 'model:fd-dev-uml-dpd', 'model:fd-dev-uml-sd', 'model:fd-dev-uml-std', 'model:fd-dev-uml-ucd', 'model:fd-dev-view', 'model:fd-diagram-link', 'model:fd-diagram', 'model:fd-dpd', 'model:fd-filelink', 'model:fd-following', 'model:fd-form-control', 'model:fd-form-view', 'model:fd-generation-step-log', 'model:fd-generation', 'model:fd-inheritance', 'model:fd-object-in-system', 'model:fd-plugin-on-rep-object', 'model:fd-project', 'model:fd-registered-plug-in', 'model:fd-repository-browser-data-object-with-a-c-l', 'model:fd-repository-browser-data-object', 'model:fd-repository-data-object', 'model:fd-repository-object-with-plugins', 'model:fd-repository-ref-data-object', 'model:fd-repository', 'model:fd-sd', 'model:fd-stage', 'model:fd-std', 'model:fd-storage-type', 'model:fd-subsystem', 'model:fd-ucd', 'model:fd-user-auth-profile', 'model:fd-user-in-stage', 'model:fd-view', 'model:i-c-s-soft-s-t-o-r-m-n-e-t-security-agent', 'service:syncer']
+    needs: ['serializer:fd-dev-view', 'transform:file', 'transform:decimal', 'transform:guid', 'transform:i-c-s-soft-s-t-o-r-m-n-e-t-access-type', 'transform:i-c-s-soft-s-t-o-r-m-n-e-t-business-audit-objects-t-write-mode', 'transform:i-c-s-soft-s-t-o-r-m-n-e-t-business-data-service-object-events', 'transform:new-platform-flexberry-web-designer-access-mode', 'transform:new-platform-flexberry-web-designer-generation-state', 'transform:s-t-o-r-m-c-a-s-e-repository-access-modifier', 'transform:s-t-o-r-m-c-a-s-e-s-t-o-r-m-n-e-t-repository-parameter-modifier', 'transform:new-platform-flexberry-web-designer-business-server-class', 'model:fd-ad', 'model:fd-aggregation', 'model:fd-application-user', 'model:fd-association', 'model:fd-auth-type', 'model:fd-base-association', 'model:fd-cad', 'model:fd-case-property', 'model:fd-class-storage-type', 'model:fd-class', 'model:fd-cod', 'model:fd-configuration', 'model:fd-dev-aggregation', 'model:fd-dev-associated-detail-view', 'model:fd-dev-association', 'model:fd-dev-attribute', 'model:fd-dev-base-association', 'model:fd-dev-class', 'model:fd-dev-control-type', 'model:fd-dev-diagram-link', 'model:fd-dev-filelink', 'model:fd-dev-form-control', 'model:fd-dev-form-view', 'model:fd-dev-inheritance', 'model:fd-dev-method', 'model:fd-dev-module-setting-type', 'model:fd-dev-module-setting', 'model:fd-dev-parameter', 'model:fd-dev-process-status', 'model:fd-dev-stage-history', 'model:fd-dev-stage', 'model:fd-dev-system', 'model:fd-dev-task', 'model:fd-dev-type-definition', 'model:fd-dev-uml-ad', 'model:fd-dev-uml-cad', 'model:fd-dev-uml-cod', 'model:fd-dev-uml-dpd', 'model:fd-dev-uml-sd', 'model:fd-dev-uml-std', 'model:fd-dev-uml-ucd', 'model:fd-dev-view', 'model:fd-diagram-link', 'model:fd-diagram', 'model:fd-dpd', 'model:fd-filelink', 'model:fd-following', 'model:fd-form-control', 'model:fd-form-view', 'model:fd-generation-step-log', 'model:fd-generation', 'model:fd-inheritance', 'model:fd-object-in-system', 'model:fd-plugin-on-rep-object', 'model:fd-project', 'model:fd-registered-plug-in', 'model:fd-repository-browser-data-object-with-a-c-l', 'model:fd-repository-browser-data-object', 'model:fd-repository-data-object', 'model:fd-repository-object-with-plugins', 'model:fd-repository-ref-data-object', 'model:fd-repository', 'model:fd-sd', 'model:fd-stage', 'model:fd-std', 'model:fd-storage-type', 'model:fd-subsystem', 'model:fd-ucd', 'model:fd-user-auth-profile', 'model:fd-user-in-stage', 'model:fd-view', 'model:i-c-s-soft-s-t-o-r-m-n-e-t-security-agent', 'service:syncer']
   });
 
   // Replace this with your real tests.
@@ -6101,47 +6101,6 @@ define('dummy/tests/unit/services/fd-sheet-service-test', ['ember-qunit'], funct
     assert.ok(service);
   });
 });
-define('dummy/tests/unit/transforms/fd-definition-test', ['ember-qunit', 'ember-flexberry-designer/objects/fd-view-attributes-property', 'ember-flexberry-designer/objects/fd-view-attributes-master', 'ember-flexberry-designer/objects/fd-view-attributes-detail'], function (_emberQunit, _fdViewAttributesProperty, _fdViewAttributesMaster, _fdViewAttributesDetail) {
-  'use strict';
-
-  (0, _emberQunit.moduleFor)('transform:fd-definition', 'Unit | Transform | fd definition');
-
-  (0, _emberQunit.test)('it exists', function (assert) {
-
-    var objectModel = Ember.A([_fdViewAttributesProperty.default.create({
-      name: 'TestProperty',
-      caption: 'Test Property',
-      path: 'pathTestProperty',
-      visible: false
-    }), _fdViewAttributesMaster.default.create({
-      name: 'TestMaster',
-      caption: 'Test Master',
-      path: 'pathTestMaster',
-      visible: true,
-      lookupType: 'standard',
-      masterPropertyName: 'TestMasterName',
-      masterCustomizationString: ''
-    }), _fdViewAttributesDetail.default.create({
-      name: 'TestDetail',
-      detailViewName: 'TestDetailD',
-      loadOnLoadAgregator: false,
-      path: '',
-      caption: 'Test Detail',
-      visible: true
-    })]);
-
-    var xml = '' + '<View>' + '<ViewPropertiesList>' + '<Item PropertyName="TestProperty" Caption="Test Property" Path="pathTestProperty" Visible="False"' + ' IsMaster="False" LookupType="default" MasterPropertyName="" MasterCustomizationString="" />' + '<Item PropertyName="TestMaster" Caption="Test Master" Path="pathTestMaster" Visible="True"' + ' IsMaster="True" LookupType="standard" MasterPropertyName="TestMasterName" MasterCustomizationString="" />' + '</ViewPropertiesList>' + '<ViewDetailsList>' + '<Item DetailName="TestDetail" DetailViewName="TestDetailD" LoadOnLoadAgregator="False" DetailPath="" DetailCaption="Test Detail" DetailVisible="True" />' + '</ViewDetailsList>' + '</View>';
-
-    var transform = this.subject();
-    assert.ok(transform);
-
-    var deserializeResult = transform.deserialize(xml);
-    assert.deepEqual(deserializeResult, objectModel, 'Definition deserialize does not work');
-
-    var serializeResult = transform.serialize(deserializeResult);
-    assert.equal(serializeResult, xml, 'Definition serialize does not work');
-  });
-});
 define('dummy/tests/unit/transforms/new-platform-flexberry-web-designer-business-server-class-test', ['ember-qunit'], function (_emberQunit) {
   'use strict';
 
@@ -6294,6 +6253,44 @@ define('dummy/tests/unit/utils/fd-containers-tree-test', ['qunit', 'ember-flexbe
 
     var serializeResult = (0, _fdContainersTree.serialize)(deserializeResult, jstreeData);
     assert.equal(serializeResult, xml, 'ConteinersTree serialize does not work');
+  });
+});
+define('dummy/tests/unit/utils/fd-definition-test', ['qunit', 'ember-flexberry-designer/objects/fd-view-attributes-property', 'ember-flexberry-designer/objects/fd-view-attributes-master', 'ember-flexberry-designer/objects/fd-view-attributes-detail', 'ember-flexberry-designer/utils/transforms-utils/fd-definition'], function (_qunit, _fdViewAttributesProperty, _fdViewAttributesMaster, _fdViewAttributesDetail, _fdDefinition) {
+  'use strict';
+
+  (0, _qunit.module)('Unit | Utility | fd definition');
+
+  (0, _qunit.test)('it exists', function (assert) {
+
+    var objectModel = Ember.A([_fdViewAttributesProperty.default.create({
+      name: 'TestProperty',
+      caption: 'Test Property',
+      path: 'pathTestProperty',
+      visible: false
+    }), _fdViewAttributesMaster.default.create({
+      name: 'TestMaster',
+      caption: 'Test Master',
+      path: 'pathTestMaster',
+      visible: true,
+      lookupType: 'standard',
+      masterPropertyName: 'TestMasterName',
+      masterCustomizationString: ''
+    }), _fdViewAttributesDetail.default.create({
+      name: 'TestDetail',
+      detailViewName: 'TestDetailD',
+      loadOnLoadAgregator: false,
+      path: '',
+      caption: 'Test Detail',
+      visible: true
+    })]);
+
+    var xml = '' + '<View>' + '<ViewPropertiesList>' + '<Item PropertyName="TestProperty" Caption="Test Property" Path="pathTestProperty" Visible="False"' + ' IsMaster="False" LookupType="default" MasterPropertyName="" MasterCustomizationString="" />' + '<Item PropertyName="TestMaster" Caption="Test Master" Path="pathTestMaster" Visible="True"' + ' IsMaster="True" LookupType="standard" MasterPropertyName="TestMasterName" MasterCustomizationString="" />' + '</ViewPropertiesList>' + '<ViewDetailsList>' + '<Item DetailName="TestDetail" DetailViewName="TestDetailD" LoadOnLoadAgregator="False" DetailPath="" DetailCaption="Test Detail" DetailVisible="True" />' + '</ViewDetailsList>' + '</View>';
+
+    var deserializeResult = (0, _fdDefinition.deserialize)(xml);
+    assert.deepEqual(deserializeResult, objectModel, 'Definition deserialize does not work');
+
+    var serializeResult = (0, _fdDefinition.serialize)(deserializeResult);
+    assert.equal(serializeResult, xml, 'Definition serialize does not work');
   });
 });
 define('dummy/tests/unit/utils/fd-deployment-diagram-primitives-test', ['qunit'], function (_qunit) {
