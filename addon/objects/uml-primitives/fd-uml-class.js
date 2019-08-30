c16ff32d3c13240c7dfacfa9ad8bbfc05a21f3ed
Sokolov Andrey <s.andrey-2011@yandex.ru>
2019-08-30 16:47:04 +0500

    Fix stereotype blur

--- addon/objects/uml-primitives/fd-uml-class.js
+++ addon/objects/uml-primitives/fd-uml-class.js
@@ -315,16 +315,19 @@ joint.shapes.flexberry.uml.ClassView = joint.shapes.flexberry.uml.PrimitiveEleme
     }.bind(this));
 
     this.$box.find('.class-stereotype-input').on('blur', function(evt) {
-      let $textarea = $(evt.target);
-      this.showNormalizedStereotypeOnInput($textarea);
-
-      let textareaText = $textarea.val();
+      let stereotypeText = $(evt.target).val();
+      let stereotype = this.normalizeStereotype(stereotypeText);
+      let rows = stereotypeText.split(/[\n\r|\r|\n]/);
+      let $stereotypeInput = this.$box.find('.class-stereotype-input');
+      $stereotypeInput.val(stereotype);
+      $stereotypeInput.prop('rows', rows.length);
       let objectModel = this.model.get('objectModel');
-      objectModel.set('stereotype', textareaText);
+      objectModel.set('stereotype', stereotype);
+      this.paper.trigger('updaterepobj', objectModel, 'stereotype', stereotype);
+      this.updateRectangles();
     }.bind(this));
 
     this.updateInputValue();
-    this.showNormalizedStereotypeOnInput(this.$box.find('.class-stereotype-input'));
 
     // Update the box position whenever the underlying model changes.
     this.model.on('change', this.updateBox, this);
@@ -513,14 +516,5 @@ joint.shapes.flexberry.uml.ClassView = joint.shapes.flexberry.uml.PrimitiveEleme
     }
 
     return stereotype;
-  },
-
-  showNormalizedStereotypeOnInput(element) {
-    let stereotypeText = element.val();
-    let stereotype = this.normalizeStereotype(stereotypeText);
-    let rows = stereotypeText.split(/[\n\r|\r|\n]/);
-    element.val(stereotype);
-    element.prop('rows', rows.length);
-    this.updateRectangles();
   }
 });
