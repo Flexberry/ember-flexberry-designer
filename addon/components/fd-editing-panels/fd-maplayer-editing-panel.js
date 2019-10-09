bbc90807f67894e9b8fdc886b0a63ffda3b420ea
Sokolov Andrey <s.andrey-2011@yandex.ru>
2019-10-09 14:28:41 +0500

    Add gis editing panels

---
+++ addon/components/fd-editing-panels/fd-maplayer-editing-panel.js
@@ -0,0 +1,15 @@
+import Component from '@ember/component';
+import layout from '../../templates/components/fd-editing-panels/fd-maplayer-editing-panel';
+
+export default Component.extend({
+  layout,
+
+  /**
+    Classes data.
+
+    @property model
+    @type Object
+    @default undefined
+  */
+  model: undefined,
+});
