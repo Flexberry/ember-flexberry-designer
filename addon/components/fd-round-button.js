9238abe266eb7c6683d488744af1e6581284f994
pepelyaeva <pepelyaevaks@gmail.com>
2019-10-28 16:26:20 +0500

    feat: Set style for base modal window (#541)

--- addon/components/fd-round-button.js
+++ addon/components/fd-round-button.js
@@ -1,8 +1,6 @@
 import FlexberryButtonComponent from 'ember-flexberry/components/flexberry-button';
 import layout from '../templates/components/fd-round-button';
-import { next } from '@ember/runloop';
 import { isNone } from '@ember/utils';
-import $ from 'jquery';
 
 export default FlexberryButtonComponent.extend({
   layout,
@@ -15,11 +13,15 @@ export default FlexberryButtonComponent.extend({
 
   colorClass: ' ',
 
+  /**
+    Components class names bindings.
+    @property classNameBindings
+    @type String[]
+  */
+  classNameBindings: ['colorClass'],
+
   didInsertElement() {
     this._super(...arguments);
-    next(() => {
-      $(this.element).addClass(this.get('colorClass'));
-    });
   },
 
   actions: {
