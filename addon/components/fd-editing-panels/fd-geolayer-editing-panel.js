45fdcaa043ab6687d3334b6d28948a698aa1e152
Sokolov Andrey <s.andrey-2011@yandex.ru>
2019-11-15 12:10:04 +0500

    Add e panel for geolayer

--- addon/components/fd-editing-panels/fd-geolayer-editing-panel.js
+++ addon/components/fd-editing-panels/fd-geolayer-editing-panel.js
@@ -1,16 +1,207 @@
 import Component from '@ember/component';
 import FdReadonlyModeMixin from '../../mixins/fd-editing-panels/fd-readonly-mode';
+import { isBlank, isNone } from '@ember/utils';
+import { computed, get, set } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { A } from '@ember/array';
+
 import layout from '../../templates/components/fd-editing-panels/fd-geolayer-editing-panel';
 
 export default Component.extend(FdReadonlyModeMixin, {
   layout,
 
   /**
+    @property store
+    @type Service
+  */
+  store: service(),
+
+  /**
+   Service that get current project contexts.
+
+   @property currentProjectContext
+   @type {Class}
+   @default service()
+   */
+  currentProjectContext: service('fd-current-project-context'),
+
+  /**
     Classes data.
 
     @property model
     @type Object
-    @default undefined
   */
   model: undefined,
+
+  /**
+    Array classes.
+
+    @property layerClasses
+    @type Array
+  */
+  layerClasses: undefined,
+
+  /**
+    Array layer styles.
+
+    @property layerStyles
+    @type Array
+  */
+  layerStyles: undefined,
+
+  /**
+    The 'coordinateSystem' of the geolayer.
+
+    @property coordinateSystem
+    @type String
+  */
+  coordinateSystem: computed('model.description', {
+    get() {
+      let settings = this.parseDascription();
+      return get(settings, 'coordinateSystem');
+    },
+
+    set(key, value) {
+      let settings = this.parseDascription();
+      set(settings, 'coordinateSystem', value);
+      this.set('model.description', JSON.stringify(settings))
+      return value;
+    }
+  }),
+
+  /**
+    The 'coverageLT' of the geolayer.
+
+    @property coverageLT
+    @type String
+  */
+  coverageLT: computed('model.description', {
+    get() {
+      let settings = this.parseDascription();
+      return get(settings, 'coverageLT');
+    },
+
+    set(key, value) {
+      let settings = this.parseDascription();
+      set(settings, 'coverageLT', value);
+      this.set('model.description', JSON.stringify(settings))
+      return value;
+    }
+  }),
+
+  /**
+    The 'coverageRD' of the geolayer.
+
+    @property coverageRD
+    @type String
+  */
+  coverageRD: computed('model.description', {
+    get() {
+      let settings = this.parseDascription();
+      return get(settings, 'coverageRD');
+    },
+
+    set(key, value) {
+      let settings = this.parseDascription();
+      set(settings, 'coverageRD', value);
+      this.set('model.description', JSON.stringify(settings))
+      return value;
+    }
+  }),
+
+  /**
+    The 'layerClass' of the geolayer.
+
+    @property layerClass
+    @type String
+  */
+  layerClass: computed('model.description', {
+    get() {
+      let settings = this.parseDascription();
+      let layerClasses = this.get('layerClasses.objects').findBy('id', get(settings, 'layerClass'));
+      return isNone(layerClasses) ? '' : layerClasses.get('name');
+    },
+
+    set(key, value) {
+      let settings = this.parseDascription();
+      let layerClasses = this.get('layerClasses.objects').findBy('name', value);
+      set(settings, 'layerClass', layerClasses.get('id'));
+      this.set('model.description', JSON.stringify(settings))
+      return value;
+    }
+  }),
+
+  /**
+    The 'layerStyle' of the geolayer.
+
+    @property layerStyle
+    @type String
+  */
+  layerStyle: computed('model.description', {
+    get() {
+      let settings = this.parseDascription();
+      let layerStyles = this.get('layerStyles.objects').findBy('id', get(settings, 'layerStyle'));
+      return isNone(layerStyles) ? '' : layerStyles.get('name');
+    },
+
+    set(key, value) {
+      let settings = this.parseDascription();
+      let layerStyles = this.get('layerStyles.objects').findBy('name', value);
+      set(settings, 'layerStyle', layerStyles.get('id'));
+      this.set('model.description', JSON.stringify(settings))
+      return value;
+    }
+  }),
+
+  /**
+    Get and parse 'description' value.
+
+    @method parseDascription
+  */
+  parseDascription() {
+    let description = this.get('model.description');
+
+    try {
+      let settings = JSON.parse(description);
+
+      return isBlank(settings) ? {} : settings;
+    } catch(e) {
+      return {};
+    }
+  },
+
+  /**
+    See [EmberJS API](https://emberjs.com/).
+
+    @method didInsertElement
+  */
+  init() {
+    this._super(...arguments);
+
+    let store = this.get('store');
+    let stagePk = this.get('currentProjectContext').getCurrentStage();
+
+    // Get current classes.
+    let allClasses = store.peekAll('fd-dev-class');
+    let classesCurrentStage = allClasses.filterBy('stage.id', stagePk);
+
+    // null or «implementation»
+    let implementations = classesCurrentStage.filter(function(item) {
+      return item.get('stereotype') === '«implementation»' || isBlank(item.get('stereotype'));
+    });
+
+    let implementationsSort = A(implementations).sortBy('name');
+    this.set('layerClasses', {
+      names: implementationsSort.mapBy('name'),
+      objects: implementationsSort,
+    });
+
+    // Geolayerstyles.
+    let geolayerstyles = classesCurrentStage.filterBy('stereotype', '«geolayerstyle»');
+    let geolayerstylesSort = A(geolayerstyles).sortBy('name');
+    this.set('layerStyles', {
+      names: geolayerstylesSort.mapBy('name'),
+      objects: geolayerstylesSort,
+    });
+  }
 });
--- addon/locales/ru/components/fd-geolayer-editing-panel.js
+++ addon/locales/ru/components/fd-geolayer-editing-panel.js
@@ -1,3 +1,12 @@
 export default {
-
+  'caption-caption': 'Заголовок',
+  'caption-placeholder': 'Введите заголовок слоя',
+  'coordinate-system-caption': 'Система координат',
+  'coordinate-system-placeholder': 'Введите систему координат',
+  'coverage-lt-caption': 'Охват слоя левая верхняя точка',
+  'coverage-lt-placeholder': 'Введите координату',
+  'coverage-rd-caption': 'Охват слоя правая нижняя точка',
+  'coverage-rd-placeholder': 'Введите координату',
+  'class-caption': 'Класс слоя',
+  'style-caption': 'Стиль слоя',
 };
