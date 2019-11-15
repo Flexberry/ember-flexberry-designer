5a8663898fe3686c4d5262d2b690d8852e452b24
s-andrey <s.andrey-2011@yandex.ru>
2019-11-15 14:19:38 +0500

    Add edit panel for geolayer (#568)

--- addon/locales/en/components/fd-geolayer-editing-panel.js
+++ addon/locales/en/components/fd-geolayer-editing-panel.js
@@ -1,3 +1,12 @@
 export default {
-
+  'caption-caption': 'Caption',
+  'caption-placeholder': 'Enter the caption of the layer',
+  'coordinate-system-caption': 'Coordinate system',
+  'coordinate-system-placeholder': 'Enter coordinate system',
+  'coverage-lt-caption': 'Coverage layer upper left point',
+  'coverage-lt-placeholder': 'Enter coordinate',
+  'coverage-rd-caption': 'Coverage layer bottom right point',
+  'coverage-rd-placeholder': 'Enter coordinate',
+  'class-caption': 'Layer class',
+  'style-caption': 'Layer style',
 };
