8d2d63ef335f20890cbf998c1419f1b406ee21d9
AntonIV8 <deimoss@mail.ru>
2020-02-14 11:34:06 +0500

    Feature update view when deleting class (#637)

--- addon/components/fd-sheets/fd-diagram-sheet.js
+++ addon/components/fd-sheets/fd-diagram-sheet.js
@@ -10,6 +10,7 @@ import { A } from '@ember/array';
 import { computed, observer, set, get } from '@ember/object';
 
 import hasChanges from '../../utils/model-has-changes';
+import { getUpdatedViews } from '../../utils/fd-update-class-diagram';
 import { updateObjectByStr } from '../../utils/fd-update-str-value';
 
 import layout from '../../templates/components/fd-sheets/fd-diagram-sheet';
@@ -192,6 +193,7 @@ export default FdBaseSheet.extend({
       });
 
       model.rollbackAll();
+      this.get('emptyReferenceCountItems').clear();
       this.set('isDiagramVisible', false);
       set(selectedValue, 'active', false);
     }
@@ -321,6 +323,13 @@ export default FdBaseSheet.extend({
 
             if (removeClasses.length > 0) {
               promises.pushObjects(removeClasses.map(mapFunction));
+
+              let updatedViews = A();
+              removeClasses.forEach(removeClasse => {
+                updatedViews.pushObjects(getUpdatedViews(store, primitives, removeClasse.get('name'), null));
+              });
+
+              promises.pushObjects(updatedViews.map(a => a.save()));
             }
 
             emptyReferenceCountItems.clear();
