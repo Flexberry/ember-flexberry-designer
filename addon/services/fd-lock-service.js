aaebd851a08ff7c83063904df0a55c088d65bb3c
AntonIV <anton-iv87@yandex.ru>
2020-03-13 16:20:20 +0500

    Fixes

--- addon/services/fd-lock-service.js
+++ addon/services/fd-lock-service.js
@@ -127,7 +127,7 @@ export default Service.extend({
      @param {String} objectTypeName Edited object type name.
   */
   deleteLock(editedObject, objectTypeName) {
-    if (editedObject && !editedObject.isNew) {
+    if (editedObject && editedObject.get('id') !== null) {
       let adapter = getOwner(this).lookup('adapter:application');
       if (objectTypeName === 'diagram-sheet') {
         const actionParams = this._createParamsForDiagramLock(editedObject);
