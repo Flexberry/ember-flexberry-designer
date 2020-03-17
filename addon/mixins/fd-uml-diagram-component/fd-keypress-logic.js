bc016109d6a9df719451349764c82ab552101e91
Sokolov Andrey <s.andrey-2011@yandex.ru>
2020-03-17 17:36:44 +0500

    Add ctrl c, v

--- addon/mixins/fd-uml-diagram-component/fd-keypress-logic.js
+++ addon/mixins/fd-uml-diagram-component/fd-keypress-logic.js
@@ -3,7 +3,14 @@
 */
 
 import Mixin from '@ember/object/mixin';
+import FdUmlLink from '../../objects/uml-primitives/fd-uml-link';
 import { inject as service } from '@ember/service';
+import { get, set } from '@ember/object';
+import { A, isArray } from '@ember/array';
+import { isNone } from '@ember/utils';
+import uuid from 'npm:node-uuid';
+import { updateObjectByStr } from '../../utils/fd-update-str-value';
+import { getDataForBuildTree } from '../../utils/fd-attributes-for-tree';
 
 /**
   Mixin with key press logic for fd-uml-diagram component.
@@ -31,16 +38,16 @@ export default Mixin.create({
     let paperEl = paper.$el;
     paperEl.attr('tabindex', 0);
 
-    paperEl.on('mouseover', this._mouseoverHandler);
+    paperEl.on('click', this._clickHandler);
     paperEl.on('keydown', this._keydownHandler.bind(this));
   },
 
   /**
-    Handler 'mouseover'.
+    Handler 'click'.
 
-    @method _mouseoverHandler
+    @method _clickHandler
   */
-  _mouseoverHandler() {
+  _clickHandler() {
     this.focus();
   },
 
@@ -52,8 +59,8 @@ export default Mixin.create({
   */
   _keydownHandler(e) {
     e.stopPropagation();
-    e.preventDefault();
 
+    let preventDefault = true;
     if (e.ctrlKey) {
       switch (e.keyCode) {
         // ctrl + a
@@ -63,14 +70,18 @@ export default Mixin.create({
 
         // ctrl + c
         case 67:
+          this.copyHighlightElements(false);
         break;
 
         // ctrl + x
         case 88:
+          this.copyHighlightElements(true);
         break;
 
         // ctrl + v
         case 86:
+          preventDefault = false;
+          this.pasteCopyElements();
         break;
 
         // ctrl + z
@@ -89,6 +100,10 @@ export default Mixin.create({
     } else if (e.keyCode === 46) {
       this.deleteSelectElements();
     }
+
+    if (preventDefault) {
+      e.preventDefault();
+    }
   },
 
   /**
@@ -104,6 +119,423 @@ export default Mixin.create({
       let cellView = paper.findViewByModel(cell);
       cellView.highlight(null, { highlightAll: true });
     });
+  },
+
+  /**
+    Handler 'ctrl + c' copy selected diagram elements.
+
+    @method copyHighlightElements
+  */
+  copyHighlightElements(isCut) {
+    let readonly = this.get('readonly');
+    if (readonly && isCut) {
+      return;
+    }
+
+    let copyPrimitive = A();
+    let copyRepObjects = A();
+
+    let highlightedAllElements = this.get('highlightedElements');
+    let highlightedElements = highlightedAllElements.filter((a) => a.model.isElement());
+    let highlightedLinks = highlightedAllElements.filter((a) => a.model.isLink());
+
+    let elements = this.get('elements');
+    highlightedElements.forEach((element) => {
+      let id = get(element, 'model.id');
+      let model = elements.findBy('id', id);
+      let repObject = model.repositoryObject;
+      if (!isNone(repObject)) {
+        let repObjectId = repObject.slice(1, -1);
+        let classObject = this.get('store').peekRecord('fd-dev-class', repObjectId);
+        copyRepObjects.pushObject({
+          type: 'fd-dev-class',
+          data: classObject,
+          views: classObject.get('views').map((a) => a.serialize({ includeId: true })),
+          formViews: classObject.get('formViews').map((a) => {
+            let serializeObj = a.serialize({ includeId: true });
+            serializeObj[`View@odata.bind`] = a.get('view.name');
+            serializeObj[`Class@odata.bind`] = a.get('class.name');
+
+            return serializeObj;
+          }),
+        });
+      }
+
+      copyPrimitive.pushObject(model.primitive);
+    });
+
+    let links = this.get('links');
+    highlightedLinks.forEach((link) => {
+      let id = get(link, 'model.id');
+      let model = links.findBy('id', id);
+      let sourceElement = copyPrimitive.findBy('$id', get(model, 'source.id'));
+      let targetElement = copyPrimitive.findBy('$id', get(model, 'target.id'));
+      if (!isNone(sourceElement) && !isNone(targetElement)) {
+        let repObject = model.repositoryObject;
+        if (!isNone(repObject)) {
+          let repObjectId = repObject.slice(1, -1);
+          let modelName = this._getModelName(model.primitive.$type);
+          let linkObject = this.get('store').peekRecord(modelName, repObjectId);
+          copyRepObjects.pushObject({
+            type: modelName,
+            data: linkObject
+          });
+        }
+
+        copyPrimitive.pushObject(model.primitive);
+      }
+    });
+
+    let copyValue = {
+      type: this.get('model.constructor.modelName'),
+      primitives: copyPrimitive,
+      repObjects: copyRepObjects.uniqBy('data.id').map((a) => {
+        a.data = a.data.serialize({ includeId: true });
+        return a;
+      })
+    };
+
+    // Create new element
+    let el = document.createElement('textarea');
+
+    // Set value (string to be copied), set non-editable to avoid focus and move outside of view
+    el.value =  JSON.stringify(copyValue);
+    el.style = { display: 'none' };
+    document.body.appendChild(el);
+
+    // Select text inside element, copy text to clipboard and remove temporary element.
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    if (isCut) {
+      this.deleteSelectElements();
+    }
+  },
+
+  /**
+    Handler 'ctrl + v'.
+
+    @method pasteCopyElements
+  */
+  pasteCopyElements() {
+    let readonly = this.get('readonly');
+    if (readonly) {
+      return;
+    }
+
+    window.addEventListener('paste', this.createPasteElementsOnDiagram.bind(this), { once: true });
+  },
+
+  /**
+    Create object on diagrams.
+
+    @method createPasteElementsOnDiagram
+    @param {Event} e event.
+  */
+  createPasteElementsOnDiagram(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    let json = e.clipboardData.getData('text/plain');
+    let model = this.get('model');
+    let pasteValue = this._parsePasteData(json);
+    if (pasteValue.type !== model.get('constructor.modelName')) {
+      return;
+    }
+
+    let primitives = pasteValue.primitives;
+    let repObjects = pasteValue.repObjects;
+
+    // Create new rep objects.
+    let store = this.get('store');
+    let listRepObject = A();
+    let dictionaryRepObjId = A();
+    repObjects.forEach((repObject) => {
+      let stage = this.get('currentProjectContext').getCurrentStageModel();
+      let allObjects = store.peekAll(repObject.type);
+      let allObjectsCurrentStage = allObjects.filterBy('stage.id', stage.get('id'));
+
+      let existObject;
+      if (repObject.type === 'fd-dev-class') {
+        existObject = allObjectsCurrentStage.findBy('name', repObject.data.Name);
+      } else {
+        let isInh = repObject.type === 'fd-dev-inheritance';
+        existObject = allObjectsCurrentStage.find((a) => {
+          let repStartObj = repObject.data[`${(isInh ? 'Parent' : 'StartClass')}@odata.bind`].slice(12, -1);
+          repStartObj = this._findNewObjectId(repStartObj, dictionaryRepObjId);
+          let repEndObj = repObject.data[`${(isInh ? 'Child' : 'EndClass')}@odata.bind`].slice(12, -1);
+          repEndObj = this._findNewObjectId(repEndObj, dictionaryRepObjId);
+          let repStartRole = repObject.data.StartRole;
+          let repEndRole = repObject.data.EndRole;
+          let repDescription = repObject.data.Description;
+          if (
+            a.get(`${(isInh ? 'parent' : 'startClass')}.id`) === repStartObj &&
+            a.get(`${(isInh ? 'child' : 'endClass')}.id`) === repEndObj &&
+            a.get(`startRole`) === repStartRole &&
+            a.get(`endRole`) === repEndRole &&
+            a.get(`description`) === repDescription
+            ) {
+              return a;
+          }
+        });
+      }
+
+      if (isNone(existObject)) {
+        store.push(store.normalize(repObject.type, repObject.data));
+        store.peekRecord(repObject.type, repObject.data.__PrimaryKey).set('stage', stage);
+        listRepObject.pushObject(repObject);
+      } else {
+        let primitiveRepObj = primitives.filter((a) => !isNone(a.RepositoryObject) && a.RepositoryObject === `{${repObject.data.__PrimaryKey}}`);
+        primitiveRepObj.map((a) => {
+          set(a, 'RepositoryObject', `{${existObject.get('id')}}`);
+          return a;
+        });
+
+        this._incrementPropertyReferenceCount(existObject);
+        dictionaryRepObjId.pushObject({
+          oldId: repObject.data.__PrimaryKey,
+          newId: existObject.get('id')
+        });
+      }
+    });
+
+    // Add primitives on diagram.
+    let dictionaryPrimitivesId = A();
+    this.get('graph').addCells(primitives.map((primitive) => {
+      let umlObject = model.createUmlObject(primitive);
+      let oldId = umlObject.get('id');
+      let newId = this._createNewPrimitiveId(oldId, dictionaryPrimitivesId);
+      umlObject.set('primitive.$id', newId);
+
+      if (umlObject instanceof FdUmlLink) {
+        let oldSourceId = umlObject.get('source.id');
+        let newSourceId = this._createNewPrimitiveId(oldSourceId, dictionaryPrimitivesId);
+        umlObject.set('primitive.StartPrimitive.$ref', newSourceId);
+
+        let oldTargetId = umlObject.get('target.id');
+        let newTargetId = this._createNewPrimitiveId(oldTargetId, dictionaryPrimitivesId);
+        umlObject.set('primitive.EndPrimitive.$ref', newTargetId);
+      }
+
+      this._addToPrimitives(umlObject);
+      let element = umlObject.JointJS();
+
+      return element;
+    }));
+
+    // Add link properties.
+    let addLinks = listRepObject.filter(a => a.type !== 'fd-dev-class');
+    addLinks.forEach((linkObj) => {
+
+      // Add start and end class.
+      let linkId = this._findNewObjectId(linkObj.data.__PrimaryKey, dictionaryRepObjId);
+      let linkRepObj = store.peekRecord(linkObj.type, linkId);
+
+      let isInh = linkObj.type === 'fd-dev-inheritance';
+      let repStartObj = linkObj.data[`${(isInh ? 'Parent' : 'StartClass')}@odata.bind`].slice(12, -1);
+      let repEndObj = linkObj.data[`${(isInh ? 'Child' : 'EndClass')}@odata.bind`].slice(12, -1);
+
+      let repStartObjId = this._findNewObjectId(repStartObj, dictionaryRepObjId);
+      let repEndObjId = this._findNewObjectId(repEndObj, dictionaryRepObjId);
+
+      let startClass = store.peekRecord('fd-dev-class', repStartObjId);
+      let endClass = store.peekRecord('fd-dev-class', repEndObjId);
+
+      linkRepObj.set(`${(isInh ? 'parent' : 'startClass')}`, startClass);
+      linkRepObj.set(`${(isInh ? 'child' : 'endClass')}`, endClass);
+    });
+
+    // Add class properties.
+    let addClasses = listRepObject.filter(a => a.type === 'fd-dev-class');
+    addClasses.forEach((classObj) => {
+
+      // Add attributes and methods.
+      let clsId = this._findNewObjectId(classObj.data.__PrimaryKey, dictionaryRepObjId);
+      let clsObj = store.peekRecord('fd-dev-class', clsId);
+      updateObjectByStr(clsObj, store);
+
+      // Add bs.
+      let bs = classObj.data[`BusinessServerClass@odata.bind`];
+      if (!isNone(bs)) {
+        let bsId = this._findNewObjectId(bs.slice(12, -1), dictionaryRepObjId);
+        let bsObj = store.peekRecord('fd-dev-class', bsId);
+
+        if (!isNone(bsObj)) {
+          clsObj.set('businessServerClass', bsObj);
+        }
+      }
+
+      // Add views.
+      let views = classObj.views;
+      views.forEach((view) => {
+        store.push(store.normalize('fd-dev-view', view));
+        let obj = store.peekRecord('fd-dev-view', view.__PrimaryKey);
+        obj.set('class', clsObj);
+
+        let definitionArray = obj.get('definitionArray');
+        let deleteDefinition = A();
+        definitionArray.forEach((definition) => {
+          let defName = definition.get('name');
+          let defNamePart = defName.split('.');
+          let currentClass = clsObj;
+          for (var i = 0; i < defNamePart.length; i++) {
+            let name = defNamePart[i];
+
+            let isExistObj;
+            if (i === defNamePart.length - 1) {
+              isExistObj = this._checkExistProp(currentClass, name, true);
+            } else {
+              isExistObj = this._checkExistProp(currentClass, name, false);
+            }
+
+            if (!isNone(isExistObj)) {
+              currentClass = isExistObj;
+            } else {
+              deleteDefinition.pushObject(definition);
+              break;
+            }
+          }
+        });
+
+        if (deleteDefinition.length > 0) {
+          definitionArray.removeObjects(deleteDefinition);
+
+          //For trigger computed propherty in fd-dev-view model.
+          obj.get('definitionArray');
+        }
+      });
+    });
+
+    // Add formViews.
+    addClasses.forEach((classObj) => {
+      let formViews = classObj.formViews;
+      formViews.forEach((formView) => {
+        let viewName = formView[`View@odata.bind`];
+        let clsName = formView[`Class@odata.bind`];
+
+        if (!isNone(viewName) && !isNone(clsName)) {
+          let allClasses = store.peekAll('fd-dev-class');
+          let allClassesCurrentStage = allClasses.filterBy('stage.id', this.get('currentProjectContext').getCurrentStage());
+          let clsObj = allClassesCurrentStage.findBy('name', clsName);
+
+          if (!isNone(clsObj)) {
+            let views = clsObj.get('views');
+            let viewObj = views.findBy('name', viewName);
+            if (!isNone(viewObj)) {
+              store.push(store.normalize('fd-dev-form-view', formView));
+              let obj = store.peekRecord('fd-dev-form-view', formView.__PrimaryKey);
+              obj.set('class', clsObj);
+              obj.set('view', viewObj);
+            }
+          }
+        }
+      });
+    });
+  },
+
+  /**
+    Add new object to diagram's primitives.
+
+    @method _addToPrimitives
+    @param {Object} umlClass
+   */
+  _addToPrimitives(umlClass) {
+    if (!umlClass) {
+      return;
+    }
+
+    let primitives = this.get('primitives');
+    if (isArray(primitives)) {
+      primitives.pushObject(umlClass);
+    } else {
+      this.set('primitives', A([ umlClass ]));
+    }
+  },
+
+  /**
+    Parse json to array diagram's primitives objects.
+
+    @method _parsePasteData
+    @param {String} json
+   */
+  _parsePasteData(json) {
+    try {
+      let pasteValue = JSON.parse(json);
+
+      return pasteValue;
+    } catch(e) {
+      return {};
+    }
+  },
+
+  /**
+    Create new id for diagram's primitives.
+
+    @method _createNewPrimitiveId
+    @param {String} oldId
+    @param {Array} dictionaryPrimitivesId
+   */
+  _createNewPrimitiveId(oldId, dictionaryPrimitivesId) {
+    let dictionaryValue = dictionaryPrimitivesId.findBy('oldId', oldId);
+    if (isNone(dictionaryValue)) {
+      let newId = `{${uuid.v4()}}`;
+      dictionaryPrimitivesId.pushObject({
+        oldId: oldId,
+        newId: newId
+      });
+
+      return newId;
+    }
+
+    return dictionaryValue.newId;
+  },
+
+  /**
+    Find new id for rep objects.
+
+    @method _findNewObjectId
+    @param {String} oldId
+    @param {Array} dictionaryId
+   */
+  _findNewObjectId(oldId, dictionaryId) {
+    let dictionaryValue = dictionaryId.findBy('oldId', oldId);
+    if (isNone(dictionaryValue)) {
+      return oldId;
+    }
+
+    return dictionaryValue.newId;
+  },
+
+  _checkExistProp(cls, name, isAttr) {
+    let dataForClass = getDataForBuildTree(this.get('store'), cls.get('id'));
+
+    let findFunction = function(item) {
+      let isAgg = item.get('constructor.modelName');
+      let cls = item.get(`${(isAgg ? 'endClass' : 'startClass')}`);
+      let value = item.get(`${(isAgg ? 'endRole' : 'startRole')}`) || cls.get('name');
+
+      return value === name;
+    }
+
+    let isExist;
+    if (isAttr) {
+      isExist = dataForClass.classes.find((cls) => {
+        let clsIsExist = cls.get('attributes').findBy('name', name);
+
+        return !isNone(clsIsExist);
+      });
+      if (isNone(isExist)) {
+        isExist = dataForClass.aggregations.find(findFunction);
+      }
+      if (isNone(isExist)) {
+        isExist = dataForClass.associations.find(findFunction);
+      }
+    } else {
+      isExist = dataForClass.associations.find(findFunction);
+    }
+
+    return isExist;
   },
 
   /**
