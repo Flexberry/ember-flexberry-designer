1a392de28a30e89e4d1d281659ce76a5fe28ad19
Pepelyaeva Kseniya <kpepelyaeva@ics.perm.ru>
2021-02-24 11:51:43 +0500

    Fix

--- addon/controllers/fd-generation-settings.js
+++ addon/controllers/fd-generation-settings.js
@@ -2,6 +2,7 @@
  * @module ember-flexberry-designer
  */
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 /**
  * The controller for the form with a generation settings.
@@ -16,79 +17,81 @@ export default Controller.extend({
    *
    * @property {object} genSettingsFile
    */
-  genSettingsFile: {
-    "ProjectId": "00000000-0000-0000-0000-000000000000",
-    "CrpLocalPath": "sample.crp",
-    "JsonMetadataPath": null,
-    "OnlineCrpUrl": null,
-    "OneShotToken": null,
-    "GenPath": "../../../src",
-    "GenerationItems": {
-      "Validate": false,
-      "Frontend": {
-        "EmberJs": {
-          "EmberApp": true,
-          "EmberAddon": false,
-          "Build": false,
-          "GitClone": false,
-          "GitPush": false,
-          "GhPagesPush": false
+  genSettingsFile: computed(function() {
+    return {
+      "ProjectId": "00000000-0000-0000-0000-000000000000",
+      "CrpLocalPath": "sample.crp",
+      "JsonMetadataPath": null,
+      "OnlineCrpUrl": null,
+      "OneShotToken": null,
+      "GenPath": "../../../src",
+      "GenerationItems": {
+        "Validate": false,
+        "Frontend": {
+          "EmberJs": {
+            "EmberApp": true,
+            "EmberAddon": false,
+            "Build": false,
+            "GitClone": false,
+            "GitPush": false,
+            "GhPagesPush": false
+          },
+          "ApacheCordova": {
+            "CordovaProject": false,
+            "BuildApp": false
+          }
         },
-        "ApacheCordova": {
-          "CordovaProject": false,
-          "BuildApp": false
+        "Backend": {
+          "AspNet": false,
+          "AspNetCore": true,
+          "Java": false
+        },
+        "Docker": {
+          "Dockerfile": false,
+          "DockerfileAutobuild": false,
+          "DockerBuildShellScript": false,
+          "DockerSwarm": false
+        },
+        "Storage": {
+          "Mssql": {
+            "DiffSql": false,
+            "FullSql": true,
+            "Security": false,
+            "AdminUser": false,
+            "ConnectionString": null
+          },
+          "PostgreSql": {
+            "DiffSql": false,
+            "FullSql": true,
+            "Security": false,
+            "AdminUser": false,
+            "ConnectionString": null
+          },
+          "ClickHouse": {
+            "DiffSql": false,
+            "FullSql": false,
+            "ConnectionString": null
+          },
+          "Oracle": {
+            "DiffSql": false,
+            "FullSql": true,
+            "ConnectionString": null
+          },
+          "Access": {
+            "DiffSql": false,
+            "FullSql": false,
+            "ConnectionString": null
+          }
+        },
+        "Docs": {
+          "Csv": false,
+          "Wmf": false
+        },
+        "Metadata": {
+          "Crp": false,
+          "Json": false
         }
-      },
-      "Backend": {
-        "AspNet": false,
-        "AspNetCore": true,
-        "Java": false
-      },
-      "Docker": {
-        "Dockerfile": false,
-        "DockerfileAutobuild": false,
-        "DockerBuildShellScript": false,
-        "DockerSwarm": false
-      },
-      "Storage": {
-        "Mssql": {
-          "DiffSql": false,
-          "FullSql": true,
-          "Security": false,
-          "AdminUser": false,
-          "ConnectionString": null
-        },
-        "PostgreSql": {
-          "DiffSql": false,
-          "FullSql": true,
-          "Security": false,
-          "AdminUser": false,
-          "ConnectionString": null
-        },
-        "ClickHouse": {
-          "DiffSql": false,
-          "FullSql": false,
-          "ConnectionString": null
-        },
-        "Oracle": {
-          "DiffSql": false,
-          "FullSql": true,
-          "ConnectionString": null
-        },
-        "Access": {
-          "DiffSql": false,
-          "FullSql": false,
-          "ConnectionString": null
-        }
-      },
-      "Docs": {
-        "Csv": false,
-        "Wmf": false
-      },
-      "Metadata": {
-        "Crp": false,
-        "Json": false
       }
     }
-  }
+  })
 })
