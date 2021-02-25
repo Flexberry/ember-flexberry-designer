28940559a9a46c2051a14ed03d952c56aaaab985
Sokolov Andrey <s.andrey-2011@yandex.ru>
2021-02-25 11:40:57 +0500

    Fix gen form

--- addon/routes/fd-generation-settings.js
+++ addon/routes/fd-generation-settings.js
@@ -2,6 +2,7 @@
   @module ember-flexberry-designer
 */
 import Route from '@ember/routing/route';
+import FdShareLoadData from '../mixins/fd-share-load-data';
 
 /**
  * Route for generation settings form.
@@ -9,4 +10,4 @@ import Route from '@ember/routing/route';
  * @class FdGenerationSettingsRoute
  * @extends Ember.Route
  */
-export default Route.extend({});
+export default Route.extend(FdShareLoadData, {});
