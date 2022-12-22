bd2c2b254479b340e20719e8c18c515191746afa
s-andrey <s.andrey-2011@yandex.ru>
2022-12-22 12:05:00 +0500

    Fix type map (#811)

--- addon/locales/en/forms/fd-data-types-map.js
+++ addon/locales/en/forms/fd-data-types-map.js
@@ -3,5 +3,6 @@ export default {
   'save-title': 'Form data was changed',
   'save-message': 'Would you like to save your changes?',
   'save-button': 'Save changes',
-  'rollback-button': 'Cancel changes'
+  'rollback-button': 'Cancel changes',
+  'parser-error': 'Error when generating a type map (failed to parse types without errors)'
 };
