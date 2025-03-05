1589a6ee13d74ce6858411fa8fc8561d6674d533
aorlov <aorlov@naoplatform.ru>
2025-03-05 11:24:05 +0500

    Activity diagram: moving the label of the object Decision making

--- addon/objects/uml-primitives/fd-uml-transition.js
+++ addon/objects/uml-primitives/fd-uml-transition.js
@@ -61,6 +61,70 @@ export let Transition = Connection.define('flexberry.uml.Transition', {
 });
 
 joint.shapes.flexberry.uml.TransitionView = NormalizedDescriptionView.extend({
+  template: [
+    '<div class="uml-link-inputs">',
+    '<textarea class="description-input" rows="1"></textarea>',
+    '<div class="input-buffer"></div>',
+    '</div>'
+  ].join(''),
+
+  initialize: function() {
+    NormalizedDescriptionView.prototype.initialize.apply(this, arguments);
+
+    // Prevent paper from handling pointerdown.
+    this.$box.find('textarea').on('mousedown click', function(evt) {
+      evt.stopPropagation();
+    });
+
+    this.$box.find('.description-input').on('blur', function(evt) {
+      this.addSquareBracketsToDescription($(evt.target));
+    }.bind(this));
+  },
+
+  addSquareBracketsToDescription($input) {
+    const description = $input.val();
+    const lines = description.split('\n');
+    const updatedDescription = lines
+      .map(line => line.startsWith('[')?`${line}]`:line.endsWith(']')?`[${line}`:`[${line}]`)
+      .join('\n');
+
+    $input.val(updatedDescription);
+    this.updateInputWidth('.description-input');
+  },
+
+  updateInputPosition(index, selector) {
+    let position = this.getLabelCoordinates(this.model.label(index).position);
+    let textarea = this.$box.find(selector)[0];
+    let textWidth = textarea.scrollWidth;
+
+    $(this.$box.find(selector)).css({
+      left: position.x - textWidth / 2,
+      top: position.y - textarea.scrollHeight / 2,
+      transform: 'rotate(' + (this.model.get('angle') || 0) + 'deg)'
+    });
+  },
+
+  updateInputWidth(selector) {
+    const textarea = this.$box.find(selector)[0];
+  
+    // Create a temporary span to change the width of the text
+    let buffer = document.createElement('span');
+    buffer.style.visibility = 'hidden';
+    buffer.style.whiteSpace = 'pre';
+    buffer.style.position = 'absolute';
+    buffer.style.font = window.getComputedStyle(textarea).font;
+    buffer.textContent = textarea.value || ' ';
+  
+    document.body.appendChild(buffer);
+    const newWidth = Math.max(buffer.offsetWidth + 1, 1);
+    document.body.removeChild(buffer);
+  
+    textarea.style.width = newWidth + 'px';
+  
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+  },
+
   setColors() {
     NormalizedDescriptionView.prototype.setColors.apply(this, arguments);
 
