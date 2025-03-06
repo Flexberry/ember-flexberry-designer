b06ce74ce2aab4e16db8a60c850008c1a4aeede9
Ruslan Bannikov <rbannikov@neoplatform.ru>
2025-03-06 14:55:11 +0500

    Fixed sequence diagram Object

--- addon/objects/uml-primitives/fd-uml-sequence-object.js
+++ addon/objects/uml-primitives/fd-uml-sequence-object.js
@@ -4,7 +4,6 @@
 
 import { computed } from '@ember/object';
 import joint from 'npm:jointjs';
-import $ from 'jquery';
 
 import FdUmlElement from './fd-uml-element';
 import { BaseObject } from './fd-uml-baseobject';
@@ -48,8 +47,13 @@ export default FdUmlElement.extend({
   @constructor
 */
 export let SequenceDiagramObject = BaseObject.define('flexberry.uml.sequencediagramObject', {
+  // Minimum width.
+  minWidth: 40,
+
+  // Minimum height.
+  minHeight: 40,
+
   attrs: {
-    rect: { 'width': 40, 'height': 40 },
     '.flexberry-uml-header-rect': { 'stroke': 'black', 'strokeWidth': '1', 'fill': '#ffffff' },
 
     '.flexberry-uml-header-text': {
@@ -65,17 +69,12 @@ export let SequenceDiagramObject = BaseObject.define('flexberry.uml.sequencediag
     }
   },
 
-  // Minimum height.
-  minHeight: 17,
+  heightPadding: 20,
 }, {
   markup: [
     '<g class="rotatable">',
-    '<g class="scalable">',
-
     '<rect class="flexberry-uml-header-rect"/>',
     '<text class="flexberry-uml-header-text"/>',
-
-    '</g>',
     '</g>'
   ].join(''),
 
@@ -101,36 +100,19 @@ joint.shapes.flexberry.uml.sequencediagramObjectView = joint.shapes.flexberry.um
     '</div>'
   ].join(''),
 
-  updateRectangles: function (resizedWidth, resizedHeight) {
-    const minWidth = this.model.attributes.minWidth;
-    const minHeight = this.model.attributes.minHeight;
-    const oldSize = this.model.size();
+  initialize: function () {
+    joint.shapes.flexberry.uml.BaseObjectView.prototype.initialize.apply(this, arguments);
+    this.updateRectangles();
+  },
 
-    let newHeight = Math.max( resizedHeight || oldSize.height, minHeight)
-    let newWidth = Math.max( resizedWidth || oldSize.width, minWidth)
-
-    let $box = this.$box;
-    let inputs =  $box.find('.class-name-input');
-    let $buffer = $box.find('.input-buffer');
-
-    inputs.each(function() {
-      let $input = $(this);
-      $buffer.css('font-weight', $input.css('font-weight'));
-      $buffer.text($input.val());
-      $input.width($buffer.width() + 1);
-      $input[0].style.marginLeft = -$input.width()/2 + 'px';
-    });
-
-    this.model.resize(newWidth, newHeight);
-    if (this.model.get('highlighted')) {
-      this.unhighlight();
-      this.highlight();
-    }
-
-    let paramsBox = this.$box.find('.params-input');
+  updateRectangles: function () {
+    joint.shapes.flexberry.uml.BaseObjectView.prototype.updateRectangles.apply(this, arguments);
+    let paramsBox = this.$box.find('.header-input');
+    let bbox = this.model.getBBox();
+    
     paramsBox.css({
-      left: newWidth/2,
-      top: newHeight + 10,
+      top: (bbox.height - paramsBox[0].offsetHeight)/2,
+      left: (bbox.width - paramsBox[0].offsetWidth)/2,
       position: 'absolute'
     });
   },
