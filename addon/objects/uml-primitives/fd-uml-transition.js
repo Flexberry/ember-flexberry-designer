3c6d630f8833464aa2d00955e6cded5101286c72
aorlov <aorlov@naoplatform.ru>
2025-03-18 09:31:15 +0500

    Correction of comments

--- addon/objects/uml-primitives/fd-uml-transition.js
+++ addon/objects/uml-primitives/fd-uml-transition.js
@@ -51,10 +51,6 @@ export default FdUmlBaseLink.extend({
   @constructor
 */
 export let Transition = Connection.define('flexberry.uml.Transition', {
-  attrs: {
-    text: { 'visibility': 'visible' },
-  }
-}, {
   initialize: function () {
     Dependency.prototype.initialize.apply(this, arguments);
   }
